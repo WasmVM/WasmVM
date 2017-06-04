8b66b86bbe54dbbbc85372c56189de4b246044d2
kevinbird61 <kevinbird61@gmail.com>
2017-06-04 22:53:19 +0800

    Add the main function and test the module.

--- Module.cpp
+++ Module.cpp
@@ -15,38 +15,79 @@ int Module::grow_memory(int page_size){
 }
 
 int Module::current_memory(){
-    return (this->page_counter-1)*64*1024 + current_loc;
+    return (this->page_counter-1)*64*1024 + this->current_loc;
 }
 
-int Module::i32_load(int loc){
-    int return_v = linear_m->at(loc);
-    return_v += ((int)linear_m->at(loc+1) << 8);
-    return_v += ((int)linear_m->at(loc+1) << 16);
-    return_v += ((int)linear_m->at(loc+1) << 24);
+uint32_t Module::i32_load(int loc){
+    uint32_t return_v = (uint8_t)this->linear_m->at(loc);
+    return_v += (uint32_t)((uint8_t)this->linear_m->at(loc+1) << 8);
+    return_v += (uint32_t)((uint8_t)this->linear_m->at(loc+2) << 16);
+    return_v += (uint32_t)((uint8_t)this->linear_m->at(loc+3) << 24);
+
+    // return the value
+    return return_v;
+}
+
+uint32_t Module::i32_load8_u(int loc){
+    // load 1 byte and set 0 to other
+    uint32_t return_v = (uint8_t)this->linear_m->at(loc);
+    // return the value
+    return return_v;
+}
+
+uint32_t Module::i32_load16_u(int loc){
+    // load 2 byte and set 0 to other
+    uint32_t return_v = (uint8_t)this->linear_m->at(loc);
+    return_v += (uint32_t)((uint8_t)this->linear_m->at(loc+1) << 8);
     // return the value
     return return_v;
 }
 
 uint64_t Module::i64_load(int loc){
-    uint64_t return_v = linear_m->at(loc);
-    return_v += ((uint64_t)linear_m->at(loc+1) << 8);
-    return_v += ((uint64_t)linear_m->at(loc+2) << 16);
-    return_v += ((uint64_t)linear_m->at(loc+3) << 24);
-    return_v += ((uint64_t)linear_m->at(loc+4) << 32);
-    return_v += ((uint64_t)linear_m->at(loc+5) << 40);
-    return_v += ((uint64_t)linear_m->at(loc+6) << 48);
-    return_v += ((uint64_t)linear_m->at(loc+7) << 54);
+    uint64_t return_v = (uint8_t)this->linear_m->at(loc);
+    return_v += (uint64_t)((uint8_t)this->linear_m->at(loc+1) << 8);
+    return_v += (uint64_t)((uint8_t)this->linear_m->at(loc+2) << 16);
+    return_v += (uint64_t)((uint8_t)this->linear_m->at(loc+3) << 24);
+    return_v += ((uint64_t)(uint8_t)this->linear_m->at(loc+4) << 32);
+    return_v += ((uint64_t)(uint8_t)this->linear_m->at(loc+5) << 40);
+    return_v += ((uint64_t)(uint8_t)this->linear_m->at(loc+6) << 48);
+    return_v += ((uint64_t)(uint8_t)this->linear_m->at(loc+7) << 54);
     // return the value
     return return_v;
 }
 
-int Module::i32_store(int value){
+uint64_t Module::i64_load8_u(int loc){
+    // load 1 byte and set 0 to other
+    uint64_t return_v = (uint8_t)this->linear_m->at(loc);
+    // return the value
+    return return_v;
+}
+
+uint64_t Module::i64_load16_u(int loc){
+    // load 1 byte and set 0 to other
+    uint64_t return_v = (uint8_t)this->linear_m->at(loc);
+    return_v += (uint64_t)((uint8_t)this->linear_m->at(loc+1) << 8);
+    // return the value
+    return return_v;
+}
+
+uint64_t Module::i64_load32_u(int loc){
+    // load 1 byte and set 0 to other
+    uint64_t return_v = (uint8_t)this->linear_m->at(loc);
+    return_v += (uint64_t)((uint8_t)this->linear_m->at(loc+1) << 8);
+    return_v += (uint64_t)((uint8_t)this->linear_m->at(loc+2) << 16);
+    return_v += ((uint64_t)(uint8_t)this->linear_m->at(loc+3) << 32);
+    // return the value
+    return return_v;
+}
+
+int Module::i32_store(uint32_t value){
     try{
         if(this->current_memory() + 4 >= (this->page_counter*64*1024)){
             // Memory shortage
             throw -1;
         }
-    }catch(const flag){
+    }catch(const uint8_t flag){
         // Notify the user, need to grow memory
         if(flag == -1){
             std::cout << "Linear Memory is starving... Using `grow_memory` to grow the size of memory." << std::endl;
@@ -54,10 +95,10 @@ int Module::i32_store(int value){
         return -1;
     }
     // if memory storage is enough, then we store
-    linear_m->at(this->current_memory()) = value && 255; 
-    linear_m->at(this->current_memory() + 1) = (value>>8) && 255;
-    linear_m->at(this->current_memory() + 2) = (value>>16) && 255;
-    linear_m->at(this->current_memory() + 3) = (value>>24) && 255;
+    this->linear_m->at(this->current_memory()) = (uint32_t)value & 255; 
+    this->linear_m->at(this->current_memory() + 1) = (uint32_t)(value>>8) & 255;
+    this->linear_m->at(this->current_memory() + 2) = (uint32_t)(value>>16) & 255;
+    this->linear_m->at(this->current_memory() + 3) = (uint32_t)(value>>24) & 255;
     this->current_loc += 4;
     return 0;
 }
@@ -68,21 +109,21 @@ int Module::i64_store(uint64_t value){
             // Memory shortage
             throw -1;
         }
-    }catch(const flag){
+    }catch(const uint8_t flag){
         // Notify the user, need to grow memory
         if(flag == -1){
             std::cout << "Linear Memory is starving... Using `grow_memory` to grow the size of memory." << std::endl;
         }
         return -1;
     }
-    linear_m->at(this->current_memory()) = value && 255; 
-    linear_m->at(this->current_memory() + 1) = (value>>8) && 255;
-    linear_m->at(this->current_memory() + 2) = (value>>16) && 255;
-    linear_m->at(this->current_memory() + 3) = (value>>24) && 255;
-    linear_m->at(this->current_memory() + 4) = (value>>32) && 255;
-    linear_m->at(this->current_memory() + 5) = (value>>40) && 255;
-    linear_m->at(this->current_memory() + 6) = (value>>48) && 255;
-    linear_m->at(this->current_memory() + 7) = (value>>54) && 255;
+    linear_m->at(this->current_memory()) = value & 255; 
+    linear_m->at(this->current_memory() + 1) = (value>>8) & 255;
+    linear_m->at(this->current_memory() + 2) = (value>>16) & 255;
+    linear_m->at(this->current_memory() + 3) = (value>>24) & 255;
+    linear_m->at(this->current_memory() + 4) = (value>>32) & 255;
+    linear_m->at(this->current_memory() + 5) = (value>>40) & 255;
+    linear_m->at(this->current_memory() + 6) = (value>>48) & 255;
+    linear_m->at(this->current_memory() + 7) = (value>>54) & 255;
     this->current_loc += 8;
     return 0;
 }
--- Module.h
+++ Module.h
@@ -14,9 +14,14 @@ public:
     int grow_memory(int page_size = 1); // grow memory (unit: page)
     int current_memory(); 
     // load & store => FIXME: support every instruction 
-    int i32_load(int loc);      // fetch data which start from loc
-    uint64_t i64_load(int loc);   
-    int i32_store(int value);   // store value into linear memory
+    uint32_t i32_load(int loc);      // fetch data which start from loc
+    uint32_t i32_load8_u(int loc);   // unsigned (load 1 byte and extend) i8 to i32 
+    uint32_t i32_load16_u(int loc);  // unsigned (load 2 byte and extend) i16 to i32
+    uint64_t i64_load(int loc);
+    uint64_t i64_load8_u(int loc);
+    uint64_t i64_load16_u(int loc);
+    uint64_t i64_load32_u(int loc);
+    int i32_store(uint32_t value);   // store value into linear memory
     int i64_store(uint64_t value);
 private:
     std::vector<char> *linear_m; // Each module deploy one linear memory
