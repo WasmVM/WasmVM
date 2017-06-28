caedf5c9b1b2adb4bf57ded6a38dbe85fb0e3954
LuisHsu <luis8404272@gmail.com>
2017-06-29 00:22:01 +0800

    Loop Inst

--- Decode.cpp
+++ Decode.cpp
@@ -10,6 +10,9 @@ void Decode::decode(Memory &memory, OperandStack &opStack, LocalStack &locals, b
   case 0x00:
     Instruction::ctrl_unreachable(opStack, locals, memory);
     break;
+  case 0x03:
+    Instruction::ctrl_loop(locals, memory.i32_load8_u(pc++));
+    break;
   case 0x0B:
     Instruction::ctrl_end(opStack, locals, halted);
     break;
@@ -38,6 +41,7 @@ void Decode::decode(Memory &memory, OperandStack &opStack, LocalStack &locals, b
     Instruction::i32_mul(opStack);
     break;
   default:
+    printf("%X ",opCode);
     throw "Not implement yet";
     break;
   }
