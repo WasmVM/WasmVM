52b08a7fc91db0aa75bec34a819cf4c1de863a1e
kevinbird61 <kevinbird61@gmail.com>
2017-08-24 00:45:35 +0800

    Basic part of I/O syscall finish.
    * Read,Write,Open,Close

--- src/Syscall/Call20.cpp
+++ src/Syscall/Call20.cpp
@@ -11,18 +11,145 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and limitations under the License.
 */
 
+#include <Call20.h>
+
 void Call::sysRead(Store &store, Stack &callStack){
-	
+    // Check value count 
+    if(coreStack.valueNum < 3){
+        throw Exception("[syscall][sys_read] No enough value in the stack.", coreStack);
+    }
+    /* Pop operand - (int) fd, (void *)buf,(size_t) count
+    */
+    // Pop out file descriptor
+    Value *fd = (Value *)coreStack.pop().data;
+    if(fd->type != i32){
+        throw Exception("[syscall][sys_read] Operand type is not i32.", coreStack);
+    }
+    // Pop out buf addr
+    Value *bufAddr = (Value *)coreStack.pop().data;
+    if(bufAddr->type != i32){
+        throw Exception("[syscall][sys_read] Operand type is not i32.", coreStack);
+    }
+    // Step1: Check memory address first 
+    if(coreStack.curFrame->moduleInst->memaddrs.size() < 1){
+        throw Exception("[syscall][sys_read] No memory exists in this module.", coreStack);
+    }
+    // Step2: Check memory
+    std::uint32_t memAddr = coreStack.curFrame->moduleInst->memaddrs.at(0);
+    if(memAddr >= store.mems.size()){
+        throw Exception("[syscall][sys_read] Memory not exists in the store.", coreStack);
+    }
+    // Step3: Get the pointer
+    char *memoryData = store.mems.at(memAddr)->data.data();
+    char *bufPtr = memoryData += bufAddr->data.i32;
+    // Pop out count
+    Value *count = (Value *)coreStack.pop().data;
+    if(count->type != i32){
+        throw Exception("[syscall][sys_read] Operand type is not i32.", coreStack);
+    }
+    // Sys_Read 
+    read((std::int32_t)fd->data.i32,(void *)bufPtr,(size_t)count->data.i32);
+    // Clean
+    delete fd;
+    delete bufAddr;
+    delete count;
 }
 
 void Call::sysWrite(Store &store, Stack &callStack){
-	
+	// Check value count 
+    if(coreStack.valueNum < 3){
+        throw Exception("[syscall][sys_write] No enough value in the stack.", coreStack);
+    }
+    /* Pop operand - (int) fd, (void *)buf,(size_t) count
+    */
+    // Pop out file descriptor
+    Value *fd = (Value *)coreStack.pop().data;
+    if(fd->type != i32){
+        throw Exception("[syscall][sys_write] Operand type is not i32.", coreStack);
+    }
+    // Pop out buf addr
+    Value *bufAddr = (Value *)coreStack.pop().data;
+    if(bufAddr->type != i32){
+        throw Exception("[syscall][sys_write] Operand type is not i32.", coreStack);
+    }
+    // Step1: Check memory address first 
+    if(coreStack.curFrame->moduleInst->memaddrs.size() < 1){
+        throw Exception("[syscall][sys_write] No memory exists in this module.", coreStack);
+    }
+    // Step2: Check memory
+    std::uint32_t memAddr = coreStack.curFrame->moduleInst->memaddrs.at(0);
+    if(memAddr >= store.mems.size()){
+        throw Exception("[syscall][sys_write] Memory not exists in the store.", coreStack);
+    }
+    // Step3: Get the pointer
+    char *memoryData = store.mems.at(memAddr)->data.data();
+    char *bufPtr = memoryData += bufAddr->data.i32;
+    // Pop out count
+    Value *count = (Value *)coreStack.pop().data;
+    if(count->type != i32){
+        throw Exception("[syscall][sys_write] Operand type is not i32.", coreStack);
+    }
+    // Sys_Read 
+    write((std::int32_t)fd->data.i32,(void *)bufPtr,(size_t)count->data.i32);
+    // Clean
+    delete fd;
+    delete bufAddr;
+    delete count;
 }
 
 void Call::sysOpen(Store &store, Stack &callStack){
-	
+	// Check value count 
+	if(coreStack.valueNum < 3){
+        throw Exception("[syscall][sys_open] No enough value in the stack.", coreStack);   
+    }
+    /* Pop operand - (char*)pathname , (int)flags
+    Using `int open(const char *pathname,int flags);` as implementation
+    */
+    // Pop out address of Pathname
+    Value *pathnameAddr = (Value *)coreStack.pop().data;
+    if(pathnameAddr->type != i32){
+        throw Exception("[syscall][sys_open] Operand type is not i32.", coreStack);
+    }
+    // Find out the body of pathname by addr
+    // Step1: Check memory address first 
+    if(coreStack.curFrame->moduleInst->memaddrs.size() < 1){
+        throw Exception("[syscall][sys_open] No memory exists in this module.", coreStack);
+    }
+    // Step2: Check memory
+    std::uint32_t memAddr = coreStack.curFrame->moduleInst->memaddrs.at(0);
+    if(memAddr >= store.mems.size()){
+        throw Exception("[syscall][sys_open] Memory not exists in the store.", coreStack);
+    }
+    // Step3: Get the pointer
+    char *memoryData = store.mems.at(memAddr)->data.data();
+    char *pathnamePtr = memoryData += pathnameAddr->data.i32;
+    // Pop out flags 
+    Value *flags = (Value *)coreStack.pop().data;
+    if(flags->type != i32){
+        throw Exception("[syscall][sys_open] Operand type is not i32.", coreStack);
+    }
+    // Sys_open , keep the returning file descriptor
+    std::int32_t ret_fd = open((const) pathnamePtr,(std::int32_t)flags->data.i32);
+    // Push back
+    coreStack.push(ret_fd);
+    // Clean 
+    delete pathname;
+    delete flags; 
 }
 
-void Call::sysClose(Store &store, Stack &callStack){
-	
+void Call::sysClose(Stack &callStack){
+    // Check value count 
+	if(coreStack.valueNum < 1){
+        throw Exception("[syscall][sys_close] No enough value in the stack.", coreStack);   
+    }
+    // Pop out file descriptor
+    Value *fd = (Value *)coreStack.pop().data;
+    if(fd->type != i32){
+        throw Exception("[syscall][sys_close] Operand type is not i32.", coreStack);
+    }
+    // Sys_Close
+    if(close((int)fd->data.i32) == -1){
+        throw Exception("[syscall][sys_close] System call failed.", coreStack);
+    }
+    delete fd;
 }
--- src/Syscall/Call20.h
+++ src/Syscall/Call20.h
@@ -1,15 +1,26 @@
 #ifndef CALL20_DEF
 #define CALL20_DEF
 
+#include <cstdlib>
+#include <cstring>
+
+extern "C"{
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+}
+
 #include <Instance.h>
 #include <Store.h>
 #include <Stack.h>
+#include <Exception.h>
 
 namespace Call{
 	void sysRead(Store &store, Stack &callStack);
 	void sysWrite(Store &store, Stack &callStack);
 	void sysOpen(Store &store, Stack &callStack);
-	void sysClose(Store &store, Stack &callStack);
+	void sysClose(Stack &callStack);
 }
 
 #endif
