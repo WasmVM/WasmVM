783799eeabe46b03d47cf93eca70eac1e5bd0ba6
LuisHsu <luishsuplus@gmail.com>
2018-10-15 00:28:59 +0800

    Runtime i32.sub

--- src/lib/core/runtime/i32_sub.c
+++ src/lib/core/runtime/i32_sub.c
@@ -1,5 +1,12 @@
 #include <core/Runtime.h>
 
 int runtime_i32_sub(Stack* stack){
+    Value *val1 = NULL, *val2 = NULL;
+    stack->entries->pop(stack->entries, &val2);
+    stack->entries->pop(stack->entries, &val1);
+    Value *result = new_i32Value(val1->value.i32 - val2->value.i32);
+    stack->entries->push(stack->entries, result);
+    free(val1);
+    free(val2);
     return 0;
 }
