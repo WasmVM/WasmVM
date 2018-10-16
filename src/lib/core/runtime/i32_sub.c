dd8312441fcc8a9fa99b8f286025bd9f3a93a014
LuisHsu <luishsuplus@gmail.com>
2018-10-16 11:49:22 +0800

    修掉 i32.add 和 i32.sub 裡的簡寫

--- src/lib/core/runtime/i32_sub.c
+++ src/lib/core/runtime/i32_sub.c
@@ -1,12 +1,11 @@
 #include <core/Runtime.h>
 
 int runtime_i32_sub(Stack* stack){
-    Value *val1 = NULL, *val2 = NULL;
-    stack->entries->pop(stack->entries, &val2);
-    stack->entries->pop(stack->entries, &val1);
-    Value *result = new_i32Value(val1->value.i32 - val2->value.i32);
-    stack->entries->push(stack->entries, result);
-    free(val1);
-    free(val2);
+    Value *value1 = NULL, *value2 = NULL;
+    stack->entries->pop(stack->entries, (void**)&value2);
+    stack->entries->pop(stack->entries, (void**)&value1);
+    stack->entries->push(stack->entries, new_i32Value(value1->value.i32 - value2->value.i32));
+    free(value1);
+    free(value2);
     return 0;
 }
--- test/unittests/lib/runtime/i32_sub_unittest.hpp
+++ test/unittests/lib/runtime/i32_sub_unittest.hpp
@@ -10,9 +10,9 @@ SKYPAT_F(Runtime_i32_sub, regular)
 {
     // Prepare
     Stack* stack = new_Stack();
-    Value *val1 = new_i32Value(5), *val2 = new_i32Value(3);
-    stack->entries->push(stack->entries, val1);
-    stack->entries->push(stack->entries, val2);
+    Value *value1 = new_i32Value(5), *value2 = new_i32Value(3);
+    stack->entries->push(stack->entries, value1);
+    stack->entries->push(stack->entries, value2);
     
     // Run
     runtime_i32_sub(stack);
