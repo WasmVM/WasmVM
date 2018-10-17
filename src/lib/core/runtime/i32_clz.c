a6683c33f132bb08354a70587acabd0c3ad89836
PunchShadow <littleuniverse24@gmail.com>
2018-10-17 17:57:41 +0800

    新增 i32_clz.c & unittest

--- src/lib/core/runtime/i32_clz.c
+++ src/lib/core/runtime/i32_clz.c
@@ -1,5 +1,22 @@
 #include <core/Runtime.h>
 
 int runtime_i32_clz(Stack* stack){
+    Value *value1 = NULL;
+    int32_t num = 1;
+    uint32_t tmp;
+
+    stack->entries->pop(stack->entries, (void**)&value1);
+    tmp = value1->value.i32;
+    // bit shift
+    if (tmp == 0) num = 32;
+    if ((tmp >> 16) == 0) { num += 16; tmp <<= 16;}
+    if ((tmp >> 8) == 0) { num += 8; tmp <<= 8;}
+    if ((tmp >> 4) == 0) { num += 4; tmp <<= 4;}
+    if ((tmp >> 2) == 0) { num += 2; tmp <<= 2;}
+    num = num - (tmp >> 31);
+    stack->entries->push(stack->entries, new_i32Value(num));
+
+    free(value1);
+    
     return 0;
 }
