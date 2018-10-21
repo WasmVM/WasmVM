bcdbd5e435c25caba0cf12c012adc645b04b4283
PunchShadow <littleuniverse24@gmail.com>
2018-10-21 16:30:12 +0800

    Add i32_const.c and its unit test

--- src/lib/core/runtime/i32_const.c
+++ src/lib/core/runtime/i32_const.c
@@ -1,5 +1,6 @@
 #include <core/Runtime.h>
 
-int runtime_i32_const(Stack* stack){
+int runtime_i32_const(Stack* stack, int32_t value1){
+    stack->entries->push(stack->entries, new_i32Value(value1))
     return 0;
 }
