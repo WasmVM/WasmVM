99393964b9e6a8e419be8d8c6d4781e2df3f609f
LuisHsu <luishsuplus@gmail.com>
2018-10-22 20:38:42 +0800

    定義指令的 Instance 標頭檔

---
+++ src/include/instance/ControlInstrInst.h
@@ -0,0 +1,15 @@
+#ifndef WASMVM_INSTANCE_NUMEFICINSTRINST_DEF
+#define WASMVM_INSTANCE_NUMEFICINSTRINST_DEF
+
+#include <stdint.h>
+#include <dataTypes/Value.h>
+#include <dataTypes/vector.h>
+#include <instance/InstrInst.h>
+
+typedef struct {
+    InstrInst parent;
+    ValueType resultType;
+    vector* indices; // uint32_t
+} NumericInstrInst;
+
+#endif
---
+++ src/include/instance/ParametricInstrInst.h
@@ -0,0 +1,8 @@
+#ifndef WASMVM_INSTANCE_PARAMETRICINSTRINST_DEF
+#define WASMVM_INSTANCE_PARAMETRICINSTRINST_DEF
+
+#include <instance/InstrInst.h>
+
+typedef InstrInst ParametricInstrInst;
+
+#endif
