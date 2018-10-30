57c3fe66ad1bcda19d4eaf22f480ba324615c388
PunchShadow <littleuniverse24@gmail.com>
2018-10-30 21:09:58 +0800

    Add i32_div_s and its unit test

---
+++ test/unittests/lib/core/runtime/i32_div_s_unittest.hpp
@@ -0,0 +1,25 @@
+#include <skypat/skypat.h>
+
+#define _Bool bool
+extern "C"{
+    #include <core/Runtime.h>
+}
+#undef _Bool
+
+SKYPAT_F(Runtime_i32_div_s, regular)
+{
+    // prepare
+    Stack* stack = new_Stack();
+    Value *value1 = new_i32Value(20); //Dividend
+    Value *value2 = new_i32Value(-3); //Divisor
+    stack->entries->push(stack->entries, value1);
+    stack->entries->push(stack->entries, value2);
+
+    // run
+    runtime_i32_div_s(stack);
+
+    // check
+    Value *check = NULL;
+    stack->entries->pop(stack->entries, (void**)&check);
+    EXPECT_EQ(check->value.i32, -6);
+}
