ccb892c79184cc6580c7e9d2614a60738c828b37
LuisHsu <luishsuplus@gmail.com>
2018-11-05 22:20:24 +0800

    實作 Loader ＆更新架構

--- src/include/Request.h
+++ src/include/Request.h
@@ -1,10 +1,10 @@
 #ifndef WASMVM_REQUEST_DEF
 #define WASMVM_REQUEST_DEF
 
-#include <dataTypes/list.h>
+#include <dataTypes/queue.h>
 
-typedef struct{
-    list* stages; // Stage*
+typedef struct Request_{
+    queue* stages; // Stage*
+    void (*free)(struct Request_* request);
 } Request;
-
 #endif
