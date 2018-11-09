382d3def01cf0847231339bfb5c9581bbb563d11
PunchShadow <littleuniverse24@gmail.com>
2018-11-09 16:54:49 +0800

    Add i32_ge_u.c and its unit test

---
+++ test/unittests/lib/core/runtime/i32_ge_u_unittest.hpp
@@ -0,0 +1,26 @@
+#include <skypat/skypat.h>
+
+#define _Bool bool
+extern "C"{
+    #include <core/Runtime.h>
+    #include <dataTypes/Value.h>
+}
+#undef _Bool
+
+SKYPAT_F(Runtime_i32_ge_u, regular)
+{
+    // prepare
+    Stack* stack = new_Stack();
+    Value *value1 = new_i32Value(-100);
+    Value *value2 = new_i32Value(10);
+    stack->entries->push(stack->entries, value1);
+    stack->entries->push(stack->entries, value2);
+
+    // run
+    runtime_i32_ge_u(stack);
+
+    // check
+    Value *check = NULL;
+    stack->entries->pop(stack->entries, (void**)&check);
+    EXPECT_EQ(check->value.i32, 1);
+}
