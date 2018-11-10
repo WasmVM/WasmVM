90caf10c263ef23446ffe570f3f3b60531e487cf
YangHau <vulxj0j8j8@gmail.com>
2018-11-10 21:56:15 +0800

    implement i32_ne.c

--- src/lib/core/runtime/i32_ne.c
+++ src/lib/core/runtime/i32_ne.c
@@ -1,5 +1,14 @@
 #include <core/Runtime.h>
+#include <dataTypes/Value.h>
+#include <stdlib.h>
 
 int runtime_i32_ne(Stack* stack){
+    Value *value1 = NULL, *value2 = NULL;
+    stack->entries->pop(stack->entries, (void**)&val2);
+    stack->entries->pop(stack->entries, (void**)&val1);
+
+    stack->entries->push(stack->entries, new_i32Value(val1->value.i32 != val2->value.i32));
+    free(value1);
+    free(value2);
     return 0;
 }
