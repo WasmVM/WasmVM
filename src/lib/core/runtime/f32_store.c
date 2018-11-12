7de8e60a1ca52bca15c4d0ea5bf6a09788b49665
徐朝逸 <luishsuplus@gmail.com>
2018-11-12 23:45:55 +0800

    在 runtime 加上 Memory 指令模板 (#116)
    Runtime 現在只有算數指令，這邊加上記憶體操作指令

---
+++ src/lib/core/runtime/f32_store.c
@@ -0,0 +1,6 @@
+#include <core/Runtime.h>
+
+int runtime_f32_store(Stack* stack, vector* mems, uint32_t offset, uint32_t align)
+{
+    return 0;
+}
---
+++ src/lib/core/runtime/f64_load.c
@@ -0,0 +1,6 @@
+#include <core/Runtime.h>
+
+int runtime_f64_load(Stack* stack, vector* mems, uint32_t offset, uint32_t align)
+{
+    return 0;
+}
