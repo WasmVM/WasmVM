09794367fea7f27df4b35a203e3f633d8cd97502
YangHau <vulxj0j8j8@gmail.com>
2018-11-17 22:18:38 +0800

    format file

--- src/lib/core/runtime/i32_popcnt.c
+++ src/lib/core/runtime/i32_popcnt.c
@@ -2,7 +2,8 @@
 #include <dataTypes/Value.h>
 #include <stdlib.h>
 
-int runtime_i32_popcnt(Stack* stack){
+int runtime_i32_popcnt(Stack* stack)
+{
     Value *value1 = NULL;
     stack->entries->pop(stack->entries, (void**)&value1);
 
