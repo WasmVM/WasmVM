a3b3502d83cd5586c8d0b654541fa57d45fc934f
yuhao.kuo <st40201@gmail.com>
2018-11-17 20:35:02 +0800

    clean i32_load_unittest.hpp code

--- test/unittests/lib/core/runtime/i32_load_unittest.hpp
+++ test/unittests/lib/core/runtime/i32_load_unittest.hpp
@@ -9,26 +9,6 @@ extern "C" {
 }
 #undef _Bool
 
-// SKYPAT_F(runtime_i32_load_8, regular)
-// {
-//     // Prepare
-//     Stack* stack = new_Stack();
-//     char str[] = "hello";
-//     vector *vector1 = new_vector(strlen(str), free);
-//     for(uint8_t lop = 0; lop < sizeof(str)/sizeof(str[0]); lop++) {
-//         vector1->push_back(vector1, (const void *)&str[lop]);   // insert data to vector
-//     }
-//     stack->entries->push(stack->entries, new_i32Value(0));  // memory address
-
-//     // Run
-//     runtime_i32_load(stack, vector1, 0, 0);
-
-//     // Check
-//     Value *check = NULL;
-//     stack->entries->pop(stack->entries, (void**)&check);
-//     EXPECT_EQ(check->value.i32, (int32_t)str[0]);
-// }
-
 SKYPAT_F(runtime_i32_load_32, regular)
 {
     // Prepare
