a16d38a32ff8663a6912cffb4aca8782a14167d7
ShaoHua Wang <oiz5201618@gmail.com>
2018-11-17 16:14:24 +0800

    實作 nop 指令 (#125)
    * 實作 nop 指令
    
    * 增加 nop 的 unittest

---
+++ test/unittests/lib/core/runtime/nop_unittest.hpp
@@ -0,0 +1,33 @@
+#include <skypat/skypat.h>
+
+#define _Bool bool
+extern "C" {
+#include <core/Runtime.h>
+#include <dataTypes/Value.h>
+}
+#undef _Bool
+
+SKYPAT_F(Runtime_nop, regular)
+{
+    Stack* stack = new_Stack();
+    int32_t check_value;
+
+    /*
+     * "insert 3", "nop"
+     * "insert -20", "nop"
+     * "call 3 / (-20)", "nop"
+     */
+
+    Value *value_1 = new_i32Value(3);
+    Value *value_2 = new_i32Value(-20);
+    Value *check = NULL;
+
+    stack->entries->push(stack->entries, value_1);
+    runtime_nop();
+    stack->entries->push(stack->entries, value_2);
+    runtime_nop();
+    runtime_i32_add(stack);
+    runtime_nop();
+    stack->entries->pop(stack->entries, (void**)&check);
+    EXPECT_EQ(check->value.i32, -17);
+}
