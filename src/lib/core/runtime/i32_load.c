f77f2e029d8b55cd5835e9349505c7a937dfae9d
yuhao.kuo <st40201@gmail.com>
2018-11-18 22:48:30 +0800

    modify i32_load runtime implement

--- src/lib/core/runtime/i32_load.c
+++ src/lib/core/runtime/i32_load.c
@@ -14,13 +14,17 @@ int runtime_i32_load(Stack* stack, vector* mems, uint32_t offset, uint32_t align
     stack->entries->pop(stack->entries, (void **)&value1);
     ea = (uint32_t)value1->value.i32 + offset;
 
-    if(ea >= mems->length) {
+    data = mems->at(mems, ea);
+
+    /**
+     * vector <i32> return value should not be NULL.
+     */
+    if(data == NULL) {
         fprintf(stderr, "over the memory range!");
         free(value1);
+        free(data);
         return -1;
     }
-
-    data = mems->at(mems, ea);
 
     stack->entries->push(stack->entries, new_i32Value(*data));
 
