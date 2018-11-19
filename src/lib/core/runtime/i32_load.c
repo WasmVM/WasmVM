86ffa49d5dbbc9ee5e5484c8ba4559a51f4f649f
Kuo Yu-Hao <yuhao-kuo@users.noreply.github.com>
2018-11-20 00:27:45 +0800

    實作 i32 load runtime function 及 unittest (#127)
    * add i32 load runtime function and unittest
    * clean i32_load_unittest.hpp code
    * modify i32_load runtime implement
    * add i32_load data check to unittest

--- src/lib/core/runtime/i32_load.c
+++ src/lib/core/runtime/i32_load.c
@@ -1,6 +1,35 @@
 #include <core/Runtime.h>
+#include <dataTypes/Value.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <stddef.h>
 
 int runtime_i32_load(Stack* stack, vector* mems, uint32_t offset, uint32_t align)
 {
+    Value *value1 = NULL;
+    int32_t *data = NULL;
+    uint32_t ea = 0;
+
+    stack->entries->pop(stack->entries, (void **)&value1);
+    ea = (uint32_t)value1->value.i32 + offset;
+
+    data = mems->at(mems, ea);
+
+    /**
+     * vector <i32> return value should not be NULL.
+     */
+    if(data == NULL) {
+        fprintf(stderr, "over the memory range!");
+        free(value1);
+        free(data);
+        return -1;
+    }
+
+    stack->entries->push(stack->entries, new_i32Value(*data));
+
+    free(value1);
+    free(data);
+
     return 0;
 }
