9cd2a3f13af10d655b1a1f1aff6c5ff36bceb7db
LuisHsu <luishsuplus@gmail.com>
2018-11-22 21:36:24 +0800

    驗證 WasmTable 和 FuncType

--- src/lib/validator/Validates.c
+++ src/lib/validator/Validates.c
@@ -1,6 +1,5 @@
 #include "Validates.h"
 
-<<<<<<< HEAD
 #include <stdint.h>
 #include <stddef.h>
 
@@ -79,9 +78,45 @@ int validate_Import(WasmImport* imports, WasmModule* module)
             return -1;
     }
 }
-=======
 int validate_FunctionType(FuncType* funcType)
 {
-    return funcType->results->length <= 1; // TODO: Unleash later
+    return !(funcType->results->length <= 1); // TODO: Unleash later
 }
->>>>>>> 定義 Validate 函式
+
+int validate_Func(WasmFunc* func)
+{
+
+}
+int validate_Expr(vector* expr)
+{
+
+}
+int validate_Table(WasmTable* table)
+{
+    uint32_t k = UINT32_MAX / 65536;
+    return !(table->min < k && ((table->max > 0) ? table->max < k && table->max >= table->min : 1));
+}
+int validate_Memory(WasmMemory* memory)
+{
+
+}
+int validate_Global(WasmGlobal* global)
+{
+
+}
+int validate_Elem(WasmElem* elem)
+{
+
+}
+int validate_Data(WasmData* data)
+{
+
+}
+int validate_Export(WasmExport* exports)
+{
+
+}
+int validate_Import(WasmImport* imports)
+{
+
+}
