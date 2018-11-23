d6d9c48a93b1cde9449b39c3cf13f44a89f26c4a
LuisHsu <luishsuplus@gmail.com>
2018-11-24 03:35:54 +0800

    讓 CMake 在找到 SkyPat 時不重編譯 SkyPat

---
+++ cmake/FindSkyPat.cmake
@@ -0,0 +1,13 @@
+find_library(SKYPAT_LIBRARIES
+    NAMES skypat
+    HINTS ${PROJECT_BINARY_DIR}/SkyPat/lib
+    NO_DEFAULT_PATH
+)
+get_filename_component(SKYPAT_LIBRARY_DIR ${SKYPAT_LIBRARIES} DIRECTORY)
+find_path(SKYPAT_INCLUDE_DIR
+    NAMES skypat/skypat.h
+    HINTS ${PROJECT_BINARY_DIR}/SkyPat/include
+    NO_DEFAULT_PATH
+)
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(SKYPAT REQUIRED_VARS SKYPAT_INCLUDE_DIR SKYPAT_LIBRARY_DIR)
