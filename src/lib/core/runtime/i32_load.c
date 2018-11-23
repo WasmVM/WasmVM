8165389dad6ae36efa7650b0bb1f4d5b2daf8faf
LuisHsu <luishsuplus@gmail.com>
2018-11-24 03:05:15 +0800

    修改 Runtime 記憶體指令的介面

--- src/lib/core/runtime/i32_load.c
+++ src/lib/core/runtime/i32_load.c
@@ -1,35 +1,24 @@
 #include <core/Runtime.h>
 #include <dataTypes/Value.h>
+#include <instance/MemInst.h>
 #include <stdint.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <stddef.h>
 
-int runtime_i32_load(Stack* stack, vector* mems, uint32_t offset, uint32_t align)
+int runtime_i32_load(Stack* stack, MemInst* memory, uint32_t offset, uint32_t align)
 {
     Value *value1 = NULL;
-    int32_t *data = NULL;
-    uint32_t ea = 0;
 
     stack->entries->pop(stack->entries, (void **)&value1);
-    ea = (uint32_t)value1->value.i32 + offset;
-
-    data = mems->at(mems, ea);
-
-    /**
-     * vector <i32> return value should not be NULL.
-     */
-    if(data == NULL) {
-        fprintf(stderr, "over the memory range!");
+    uint32_t ea = (uint32_t)value1->value.i32 + offset;
+    if(ea >= memory->max * 65536) {
+        fprintf(stderr, "over the memory range! \n");
         free(value1);
-        free(data);
         return -1;
     }
-
+    int32_t* data = (int32_t*)(memory->data->data + ea);
     stack->entries->push(stack->entries, new_i32Value(*data));
-
     free(value1);
-    free(data);
-
     return 0;
 }
--- src/lib/core/runtime/i32_store16.c
+++ src/lib/core/runtime/i32_store16.c
@@ -1,6 +1,6 @@
 #include <core/Runtime.h>
 
-int runtime_i32_store16(Stack* stack, vector* mems, uint32_t offset, uint32_t align)
+int runtime_i32_store16(Stack* stack, MemInst* memory, uint32_t offset, uint32_t align)
 {
     return 0;
 }
