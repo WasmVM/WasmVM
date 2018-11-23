9cf99dec7d5f8d6ef9554820685f424ce2a96f68
徐朝逸 <luishsuplus@gmail.com>
2018-11-24 07:48:53 +0800

    修改 Runtime 記憶體指令的介面 (#137)

--- src/lib/core/runtime/f32_store.c
+++ src/lib/core/runtime/f32_store.c
@@ -1,6 +1,6 @@
 #include <core/Runtime.h>
 
-int runtime_f32_store(Stack* stack, vector* mems, uint32_t offset, uint32_t align)
+int runtime_f32_store(Stack* stack, MemInst* memory, uint32_t offset, uint32_t align)
 {
     return 0;
 }
--- src/lib/core/runtime/i64_store8.c
+++ src/lib/core/runtime/i64_store8.c
@@ -1,6 +1,6 @@
 #include <core/Runtime.h>
 
-int runtime_i64_store8(Stack* stack, vector* mems, uint32_t offset, uint32_t align)
+int runtime_i64_store8(Stack* stack, MemInst* memory, uint32_t offset, uint32_t align)
 {
     return 0;
 }
