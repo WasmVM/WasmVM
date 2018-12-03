2577e03e132a967c4da3b34812dc46617036737c
HowJMay <vulxj0j8j8@gmail.com>
2018-12-03 10:27:29 +0800

    implement I32 load16 u (#144)
    * implement i32_load16_u
    
    * implement i32_load16_u
    
    * format file
    
    * implement i32_load16_u
    
    * format file
    
    * fix merge conflict

--- src/lib/core/runtime/i32_load16_u.c
+++ src/lib/core/runtime/i32_load16_u.c
@@ -1,6 +1,22 @@
 #include <core/Runtime.h>
+#include <dataTypes/Value.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <stddef.h>
 
 int runtime_i32_load16_u(Stack* stack, MemInst* memory, uint32_t offset, uint32_t align)
 {
+    Value *value1 = NULL;
+    stack->entries->pop(stack->entries, (void **)&value1);
+    uint32_t ea = (uint32_t)value1->value.i32 + offset;
+    if(ea >= memory->max * 65536) {
+        fprintf(stderr, "over the memory range! \n");
+        free(value1);
+        return -1;
+    }
+    uint16_t* data = (uint16_t*)(memory->data->data + ea);
+    stack->entries->push(stack->entries, new_i32Value(*data));
+    free(value1);
     return 0;
 }
