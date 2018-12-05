78fc013a714cc3fed67fc88400b6f63750a6f90b
Kuo Yu-Hao <yuhao-kuo@users.noreply.github.com>
2018-12-06 00:07:11 +0800

    implement i32_load8_s runtime function and unittest (#147)

--- src/lib/core/runtime/i32_load8_s.c
+++ src/lib/core/runtime/i32_load8_s.c
@@ -1,5 +1,6 @@
 #include <core/Runtime.h>
 #include <dataTypes/Value.h>
+#include <instance/MemInst.h>
 #include <stdint.h>
 #include <stdlib.h>
 #include <stdio.h>
@@ -7,5 +8,16 @@
 
 int runtime_i32_load8_s(Stack* stack, MemInst* memory, uint32_t offset, uint32_t align)
 {
+    Value *value1 = NULL;
+    stack->entries->pop(stack->entries, (void **)&value1);
+    uint32_t ea = (uint32_t)value1->value.i32 + offset;
+    if(ea >= memory->max * 65536) {
+        fprintf(stderr, "over the memory range! \n");
+        free(value1);
+        return -1;
+    }
+    int8_t* data = (int8_t*)(memory->data->data + ea);
+    stack->entries->push(stack->entries, new_i32Value(*data));
+    free(value1);
     return 0;
 }
