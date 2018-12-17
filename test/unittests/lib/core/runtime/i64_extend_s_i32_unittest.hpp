3be0e0bac6a957b5bd6f83f1a08d6faffa2e33b2
JasonKao <r05921104@ntu.edu.tw>
2018-12-18 01:37:44 +0800

    implement i64_extend_s_i32 runtime function and its unittest (#150)

---
+++ test/unittests/lib/core/runtime/i64_extend_s_i32_unittest.hpp
@@ -0,0 +1,24 @@
+#include <skypat/skypat.h>
+
+#define _Bool bool
+extern "C" {
+#include <dataTypes/Value.h>
+#include <core/Runtime.h>
+}
+#undef _Bool
+
+SKYPAT_F(Runtime_i64_extend_s_i32, regular)
+{
+    // Prepare
+    Stack* stack = new_Stack();
+    Value *value1 = new_i32Value(-20);
+    stack->entries->push(stack->entries, value1);
+
+    // Run
+    runtime_i64_extend_s_i32(stack);
+
+    // Check
+    Value *check = NULL;
+    stack->entries->pop(stack->entries, (void**)&check);
+    EXPECT_EQ(check->value.i64, -20);
+}
