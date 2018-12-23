d9019653e7da3ae5faa7272a987fdb0c3bff96fa
r05921104 <r05921104@ntu.edu.tw>
2018-12-23 10:34:53 +0800

    implement i64_extend_u_i32 runtime function and its unittest

--- src/lib/core/runtime/i64_extend_u_i32.c
+++ src/lib/core/runtime/i64_extend_u_i32.c
@@ -1,6 +1,13 @@
 #include <core/Runtime.h>
+#include <dataTypes/Value.h>
+
+#include <stdlib.h>
 
 int runtime_i64_extend_u_i32(Stack* stack)
 {
+    Value *value1 = NULL;
+    stack->entries->pop(stack->entries, (void**)&value1);
+    stack->entries->push(stack->entries, new_i64Value((uint32_t)(value1->value.i32)));
+    free(value1);
     return 0;
 }
