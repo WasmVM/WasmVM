352ac95c1a8db58ff0fb1753f2e3019c7b024573
LuisHsu <luishsuplus@gmail.com>
2018-12-24 20:47:26 +0800

    修掉型別不對的 Warning

--- src/lib/validator/ExprValidate.c
+++ src/lib/validator/ExprValidate.c
@@ -1,5 +1,6 @@
 #include "Validates.h"
 
+#include <stdlib.h>
 #include <dataTypes/stack.h>
 #include <Opcodes.h>
 
@@ -70,31 +71,31 @@ int validate_Expr(vector* expr, Context* context)
                 result = validate_Instr_call_indirect((WasmControlInstr*)instr, context, opds, ctrls);
                 break;
             case Op_drop:
-                result = validate_Instr_drop((WasmControlInstr*)instr, context, opds, ctrls);
+                result = validate_Instr_drop((WasmParametricInstr*)instr, context, opds, ctrls);
                 break;
             case Op_select:
-                result = validate_Instr_select((WasmControlInstr*)instr, context, opds, ctrls);
+                result = validate_Instr_select((WasmParametricInstr*)instr, context, opds, ctrls);
                 break;
             case Op_get_local:
-                result = validate_Instr_get_local((WasmControlInstr*)instr, context, opds, ctrls);
+                result = validate_Instr_get_local((WasmVariableInstr*)instr, context, opds, ctrls);
                 break;
             case Op_set_local:
-                result = validate_Instr_set_local((WasmControlInstr*)instr, context, opds, ctrls);
+                result = validate_Instr_set_local((WasmVariableInstr*)instr, context, opds, ctrls);
                 break;
             case Op_tee_local:
-                result = validate_Instr_tee_local((WasmControlInstr*)instr, context, opds, ctrls);
+                result = validate_Instr_tee_local((WasmVariableInstr*)instr, context, opds, ctrls);
                 break;
             case Op_get_global:
-                result = validate_Instr_get_global((WasmControlInstr*)instr, context, opds, ctrls);
+                result = validate_Instr_get_global((WasmVariableInstr*)instr, context, opds, ctrls);
                 break;
             case Op_set_global:
-                result = validate_Instr_set_global((WasmControlInstr*)instr, context, opds, ctrls);
+                result = validate_Instr_set_global((WasmVariableInstr*)instr, context, opds, ctrls);
                 break;
             case Op_i32_load:
             case Op_i64_load:
             case Op_f32_load:
             case Op_f64_load:
-                result = validate_Instr_load((WasmControlInstr*)instr, context, opds, ctrls);
+                result = validate_Instr_load((WasmMemoryInstr*)instr, context, opds, ctrls);
                 break;
             case Op_i32_load8_s:
             case Op_i32_load8_u:
@@ -106,35 +107,35 @@ int validate_Expr(vector* expr, Context* context)
             case Op_i64_load16_u:
             case Op_i64_load32_s:
             case Op_i64_load32_u:
-                result = validate_Instr_loadN((WasmControlInstr*)instr, context, opds, ctrls);
+                result = validate_Instr_loadN((WasmMemoryInstr*)instr, context, opds, ctrls);
                 break;
             case Op_i32_store:
             case Op_i64_store:
             case Op_f32_store:
             case Op_f64_store:
-                result = validate_Instr_store((WasmControlInstr*)instr, context, opds, ctrls);
+                result = validate_Instr_store((WasmMemoryInstr*)instr, context, opds, ctrls);
                 break;
             case Op_i32_store8:
             case Op_i32_store16:
             case Op_i64_store8:
             case Op_i64_store16:
             case Op_i64_store32:
-                result = validate_Instr_storeN((WasmControlInstr*)instr, context, opds, ctrls);
+                result = validate_Instr_storeN((WasmMemoryInstr*)instr, context, opds, ctrls);
                 break;
             case Op_memory_size:
-                result = validate_Instr_memory_size((WasmControlInstr*)instr, context, opds, ctrls);
+                result = validate_Instr_memory_size((WasmMemoryInstr*)instr, context, opds, ctrls);
                 break;
             case Op_memory_grow:
-                result = validate_Instr_memory_grow((WasmControlInstr*)instr, context, opds, ctrls);
+                result = validate_Instr_memory_grow((WasmMemoryInstr*)instr, context, opds, ctrls);
                 break;
             case Op_i32_const:
             case Op_i64_const:
             case Op_f32_const:
             case Op_f64_const:
-                result = validate_Instr_const((WasmControlInstr*)instr, context, opds, ctrls);
+                result = validate_Instr_const((WasmNumericInstr*)instr, context, opds, ctrls);
                 break;
             case Op_i32_eqz:
-                result = validate_Instr_testop((WasmControlInstr*)instr, context, opds, ctrls);
+                result = validate_Instr_testop((WasmNumericInstr*)instr, context, opds, ctrls);
                 break;
             case Op_i32_eq:
             case Op_i32_ne:
@@ -146,10 +147,10 @@ int validate_Expr(vector* expr, Context* context)
             case Op_i32_le_u:
             case Op_i32_ge_s:
             case Op_i32_ge_u:
-                result = validate_Instr_relop((WasmControlInstr*)instr, context, opds, ctrls);
+                result = validate_Instr_relop((WasmNumericInstr*)instr, context, opds, ctrls);
                 break;
             case Op_i64_eqz:
-                result = validate_Instr_testop((WasmControlInstr*)instr, context, opds, ctrls);
+                result = validate_Instr_testop((WasmNumericInstr*)instr, context, opds, ctrls);
                 break;
             case Op_i64_eq:
             case Op_i64_ne:
@@ -173,12 +174,12 @@ int validate_Expr(vector* expr, Context* context)
             case Op_f64_gt:
             case Op_f64_le:
             case Op_f64_ge:
-                result = validate_Instr_testop((WasmControlInstr*)instr, context, opds, ctrls);
+                result = validate_Instr_testop((WasmNumericInstr*)instr, context, opds, ctrls);
                 break;
             case Op_i32_clz:
             case Op_i32_ctz:
             case Op_i32_popcnt:
-                result = validate_Instr_unop((WasmControlInstr*)instr, context, opds, ctrls);
+                result = validate_Instr_unop((WasmNumericInstr*)instr, context, opds, ctrls);
                 break;
             case Op_i32_add:
             case Op_i32_sub:
@@ -195,12 +196,12 @@ int validate_Expr(vector* expr, Context* context)
             case Op_i32_shr_u:
             case Op_i32_rotl:
             case Op_i32_rotr:
-                result = validate_Instr_binop((WasmControlInstr*)instr, context, opds, ctrls);
+                result = validate_Instr_binop((WasmNumericInstr*)instr, context, opds, ctrls);
                 break;
             case Op_i64_clz:
             case Op_i64_ctz:
             case Op_i64_popcnt:
-                result = validate_Instr_unop((WasmControlInstr*)instr, context, opds, ctrls);
+                result = validate_Instr_unop((WasmNumericInstr*)instr, context, opds, ctrls);
                 break;
             case Op_i64_add:
             case Op_i64_sub:
@@ -217,7 +218,7 @@ int validate_Expr(vector* expr, Context* context)
             case Op_i64_shr_u:
             case Op_i64_rotl:
             case Op_i64_rotr:
-                result = validate_Instr_binop((WasmControlInstr*)instr, context, opds, ctrls);
+                result = validate_Instr_binop((WasmNumericInstr*)instr, context, opds, ctrls);
                 break;
             case Op_f32_abs:
             case Op_f32_neg:
@@ -226,7 +227,7 @@ int validate_Expr(vector* expr, Context* context)
             case Op_f32_trunc:
             case Op_f32_nearest:
             case Op_f32_sqrt:
-                result = validate_Instr_unop((WasmControlInstr*)instr, context, opds, ctrls);
+                result = validate_Instr_unop((WasmNumericInstr*)instr, context, opds, ctrls);
                 break;
             case Op_f32_add:
             case Op_f32_sub:
@@ -235,7 +236,7 @@ int validate_Expr(vector* expr, Context* context)
             case Op_f32_min:
             case Op_f32_max:
             case Op_f32_copysign:
-                result = validate_Instr_binop((WasmControlInstr*)instr, context, opds, ctrls);
+                result = validate_Instr_binop((WasmNumericInstr*)instr, context, opds, ctrls);
                 break;
             case Op_f64_abs:
             case Op_f64_neg:
@@ -244,7 +245,7 @@ int validate_Expr(vector* expr, Context* context)
             case Op_f64_trunc:
             case Op_f64_nearest:
             case Op_f64_sqrt:
-                result = validate_Instr_unop((WasmControlInstr*)instr, context, opds, ctrls);
+                result = validate_Instr_unop((WasmNumericInstr*)instr, context, opds, ctrls);
                 break;
             case Op_f64_add:
             case Op_f64_sub:
@@ -253,7 +254,7 @@ int validate_Expr(vector* expr, Context* context)
             case Op_f64_min:
             case Op_f64_max:
             case Op_f64_copysign:
-                result = validate_Instr_binop((WasmControlInstr*)instr, context, opds, ctrls);
+                result = validate_Instr_binop((WasmNumericInstr*)instr, context, opds, ctrls);
                 break;
             case Op_i32_wrap_i64:
             case Op_i32_trunc_s_f32:
@@ -276,7 +277,7 @@ int validate_Expr(vector* expr, Context* context)
             case Op_f64_convert_s_i64:
             case Op_f64_convert_u_i64:
             case Op_f64_promote_f32:
-                result = validate_Instr_cvtop((WasmControlInstr*)instr, context, opds, ctrls);
+                result = validate_Instr_cvtop((WasmNumericInstr*)instr, context, opds, ctrls);
                 break;
             default:
                 clean(opds, ctrls);
