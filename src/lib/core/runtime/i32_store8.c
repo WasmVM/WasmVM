de88960bcf535f63381cfc1b43d7a6208bcd5fd8
yuhao.kuo <st40201@gmail.com>
2019-01-05 12:43:58 +0800

    add i32 store8 runtime function and unittest

--- src/lib/core/runtime/i32_store8.c
+++ src/lib/core/runtime/i32_store8.c
@@ -1,6 +1,37 @@
 #include <core/Runtime.h>
+#include <dataTypes/Value.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <stdio.h>
 
 int runtime_i32_store8(Stack* stack, MemInst* memory, uint32_t offset, uint32_t align)
 {
+    Value *value1 = NULL;
+    Value *value2 = NULL;
+    uint8_t *dataPointer = NULL;
+    uint32_t ea = 0;
+
+    // pop data form stack
+    stack->entries->pop(stack->entries, (void **)&value2);
+    stack->entries->pop(stack->entries, (void **)&value1);
+
+    // check memory range
+    ea = (uint32_t)value1->value.i32 + offset;
+    if(ea >= memory->max * 65536) {
+        fprintf(stderr, "over the memory range! \n");
+        free_Value(value1);
+        free_Value(value2);
+        return -1;
+    }
+
+    // store data to memory
+    dataPointer = (uint8_t *)memory->data->data;
+    *(dataPointer + ea) = (uint8_t)value2->value.i32;
+
+    // free object
+    free(value1);
+    free(value2);
+
     return 0;
 }
