c9eb2523b585294a11c17f5fce56421cba4b88da
Naetw <wangzhelee.cs04@g2.nctu.edu.tw>
2019-01-16 16:42:51 +0800

    add i64_load16_s runtime function and unittest

--- src/lib/core/runtime/i64_load16_s.c
+++ src/lib/core/runtime/i64_load16_s.c
@@ -1,6 +1,27 @@
 #include <core/Runtime.h>
+#include <dataTypes/Value.h>
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-int runtime_i64_load16_s(Stack* stack, MemInst* memory, uint32_t offset, uint32_t align)
+int runtime_i64_load16_s(Stack *stack, MemInst *memory, uint32_t offset,
+                         uint32_t align)
 {
+    Value *value1 = NULL;
+
+    stack->entries->pop(stack->entries, (void **)&value1);
+
+    uint32_t ea = (uint64_t)value1->value.i32 + offset;
+
+    if (ea + (16 / 8) >= memory->max * 65536) {
+        fprintf(stderr, "over the memory range! \n");
+        free(value1);
+        return -1;
+    }
+
+    int16_t *data = (int16_t *)((uint8_t *)memory->data->data + ea);
+
+    stack->entries->push(stack->entries, new_i64Value((int64_t)*data));
+    free(value1);
     return 0;
 }
