90111e726303564bd90b75f481253eedff452d0f
Kuo Yu-Hao <yuhao-kuo@users.noreply.github.com>
2019-01-17 09:05:33 +0800

    修正 i32_store 指令存入位置 bug (#165)
    * fix i32_store store to memory address bug
    
    * fix i32_store16 address bug and unittest
    
    * fix i32_store8 bug and unittest

--- src/lib/core/runtime/i32_store8.c
+++ src/lib/core/runtime/i32_store8.c
@@ -9,7 +9,7 @@ int runtime_i32_store8(Stack* stack, MemInst* memory, uint32_t offset, uint32_t 
 {
     Value *value1 = NULL;
     Value *value2 = NULL;
-    uint8_t *dataPointer = NULL;
+    int8_t *dataPointer = NULL;
     uint32_t ea = 0;
 
     // pop data form stack
@@ -18,7 +18,7 @@ int runtime_i32_store8(Stack* stack, MemInst* memory, uint32_t offset, uint32_t 
 
     // check memory range
     ea = (uint32_t)value1->value.i32 + offset;
-    if(ea >= memory->max * 65536) {
+    if(ea + 1 >= memory->max * 65536) {
         fprintf(stderr, "over the memory range! \n");
         free_Value(value1);
         free_Value(value2);
@@ -26,8 +26,8 @@ int runtime_i32_store8(Stack* stack, MemInst* memory, uint32_t offset, uint32_t 
     }
 
     // store data to memory
-    dataPointer = (uint8_t *)memory->data->data;
-    *(dataPointer + ea) = (uint8_t)value2->value.i32;
+    dataPointer = (int8_t *)((uint8_t *)memory->data->data + ea);
+    *dataPointer = (int8_t)value2->value.i32;
 
     // free object
     free(value1);
