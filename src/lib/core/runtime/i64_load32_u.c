1cd0ef4b521bb23090e7c463395b6291e8ac5409
Ken Dai <metalhead.ken77@gmail.com>
2019-01-18 01:32:01 +0800

    implement i64_load16_u and i64_load32_u (#169)
    * add i64_load16_u runtime function and unittest
    * add i64_load32_u runtime function and unittest
    * fix i64_load16_s unittest push wrong data type in stack

--- src/lib/core/runtime/i64_load32_u.c
+++ src/lib/core/runtime/i64_load32_u.c
@@ -1,6 +1,26 @@
 #include <core/Runtime.h>
+#include <dataTypes/Value.h>
+#include <instance/MemInst.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <stddef.h>
 
 int runtime_i64_load32_u(Stack* stack, MemInst* memory, uint32_t offset, uint32_t align)
 {
+    Value *value1 = NULL;
+    stack->entries->pop(stack->entries, (void **) &value1);
+    uint32_t ea = (uint32_t) value1->value.u32 + offset;
+
+    if (ea + (32 / 8) >= memory->max * 65536) {
+        fprintf(stderr, "over the memory range! \n");
+        free(value1);
+        return -1;
+    }
+
+    uint32_t *data = (uint32_t *) ((uint8_t *) memory->data->data + ea);
+    stack->entries->push(stack->entries, new_i64Value(*data));
+    free(value1);
+
     return 0;
 }
