ef3b13adc3594e4d4c060e8fb788103b13ff67ed
Ken Dai <metalhead.ken77@gmail.com>
2019-01-28 16:06:46 +0800

    add f32_load runtime function and unittest (#176)

--- src/lib/core/runtime/f32_load.c
+++ src/lib/core/runtime/f32_load.c
@@ -1,6 +1,26 @@
 #include <core/Runtime.h>
+#include <dataTypes/Value.h>
+#include <instance/MemInst.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <stddef.h>
 
 int runtime_f32_load(Stack* stack, MemInst* memory, uint32_t offset, uint32_t align)
 {
+    Value *value1 = NULL;
+    stack->entries->pop(stack->entries, (void **) &value1);
+    uint32_t ea = (uint32_t) value1->value.u32 + offset;
+
+    if (ea + (32 / 8) >= memory->max * 65536) {
+        fprintf(stderr, "over the memory range! \n");
+        free(value1);
+        return -1;
+    }
+
+    float *data = (float *) ((uint8_t *) memory->data->data + ea);
+    stack->entries->push(stack->entries, new_f32Value(*data));
+    free(value1);
+
     return 0;
 }
