d31ada7065f5cf826dc3a0c13c91814d3493134e
YanJiun <jiun113241@gmail.com>
2019-01-30 15:26:56 +0800

    implement drop instruction and unittest

--- src/lib/core/runtime/drop.c
+++ src/lib/core/runtime/drop.c
@@ -1,6 +1,11 @@
 #include <core/Runtime.h>
+#include <dataTypes/Entry.h>
+#include <stddef.h>
+#include <dataTypes/Value.h>
 
 int runtime_drop(Stack* stack)
 {
-    return 0;
+    Value *value = NULL;
+    int ans = stack->entries->pop(stack->entries, (void**)&value);
+    return ans;
 }
