d7051b174f2427722c2165a66e9b4f8a985a06da
YanJiun <jiun113241@gmail.com>
2019-02-02 15:14:20 +0800

    implement select instruction and unittest

--- src/lib/core/runtime/select.c
+++ src/lib/core/runtime/select.c
@@ -1,6 +1,18 @@
 #include <core/Runtime.h>
+#include <dataTypes/Value.h>
+#include <stddef.h>
 
 int runtime_select(Stack* stack)
 {
+    Value *value1, *value2, *value3;
+    int err = 0;
+    err |= stack->entries->pop(stack->entries, (void**)&value1);
+    err |= stack->entries->pop(stack->entries, (void**)&value2);
+    err |= stack->entries->pop(stack->entries, (void**)&value3);
+    if(err) return -1;
+    if(value3->value.i32)
+        stack->entries->push(stack->entries, value2);
+    else
+        stack->entries->push(stack->entries, value1);
     return 0;
 }
---
+++ test/unittests/lib/core/runtime/select_unittest.hpp
@@ -0,0 +1,53 @@
+#include <skypat/skypat.h>
+
+#define _Bool bool
+extern "C" {
+#include <core/Runtime.h>
+#include <dataTypes/Value.h>
+}
+#undef _Bool
+
+SKYPAT_F(Runtime_nop, regular)
+{
+    Stack* stack = new_Stack((void (*)(void*))free_Value);
+    int32_t check_value;
+
+    Value *value_1 = new_i32Value(0);
+    Value *value_2 = new_i32Value(1);
+    Value *value_3 = new_i32Value(2);
+    Value *value_4 = new_i32Value(3);
+    Value *value_5 = new_i32Value(4);
+    Value *value_6 = new_i32Value(5);
+    Value *check = NULL;
+
+    stack->entries->push(stack->entries, value_1);
+    stack->entries->push(stack->entries, value_2);
+    stack->entries->push(stack->entries, value_3);
+    stack->entries->push(stack->entries, value_4);
+    stack->entries->push(stack->entries, value_5);
+    stack->entries->push(stack->entries, value_6);
+    EXPECT_EQ(stack->entries->size, 6);
+
+    check_value = runtime_select(stack);
+    EXPECT_EQ(check_value,0);
+    EXPECT_EQ(stack->entries->size, 4);
+
+    stack->entries->pop(stack->entries, (void**)&check);
+    EXPECT_EQ(check->value.i32, 4);
+    EXPECT_EQ(stack->entries->size, 3);
+
+    check_value = runtime_select(stack);
+    EXPECT_EQ(check_value,0);
+    EXPECT_EQ(stack->entries->size, 1);
+
+    //stack->entries->pop(stack->entries, (void**)&check);
+    //EXPECT_EQ(check->value.i32, 2);
+    //EXPECT_EQ(stack->entries->size, 0);
+
+    check_value = runtime_select(stack);
+    EXPECT_EQ(check_value, -1);
+
+    // clean
+    free_Value(check);
+    free_Stack(stack);
+}
