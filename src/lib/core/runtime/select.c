e248af14c66852dcf8a1f67df96c27df054d1892
YanJiun <1104105308@gm.kuas.edu.tw>
2019-02-18 10:49:38 +0800

    實作 runtime 的 select 和 unittest_select (#180)
    * implement drop instruction and unittest
    
    * implement drop instruction and unittest (#178)
    
    * 修正 vector free 的記憶體洩漏 (#177)
    
    * implement select instruction and unittest
    
    * fix the bug of select and select_unittest

--- src/lib/core/runtime/select.c
+++ src/lib/core/runtime/select.c
@@ -1,6 +1,18 @@
 #include <core/Runtime.h>
+#include <dataTypes/Value.h>
+#include <stddef.h>
 
 int runtime_select(Stack* stack)
 {
+    Value *value1, *value2, *value3;
+    int err = 0;
+    err |= stack->entries->pop(stack->entries, (void**)&value3);
+    err |= stack->entries->pop(stack->entries, (void**)&value2);
+    err |= stack->entries->pop(stack->entries, (void**)&value1);
+    if(err) return -1;
+    if(value3->value.i32)
+        stack->entries->push(stack->entries, value1);
+    else
+        stack->entries->push(stack->entries, value2);
     return 0;
 }
