4fbd7f2d211dee3289694d8f4978ef88d04d1e04
YanJiun <jiun113241@gmail.com>
2019-02-23 21:40:10 +0800

    Fix select's memory leak bug

--- src/lib/core/runtime/select.c
+++ src/lib/core/runtime/select.c
@@ -5,14 +5,18 @@
 int runtime_select(Stack* stack)
 {
     Value *value1, *value2, *value3;
-    int err = 0;
-    err |= stack->entries->pop(stack->entries, (void**)&value3);
-    err |= stack->entries->pop(stack->entries, (void**)&value2);
-    err |= stack->entries->pop(stack->entries, (void**)&value1);
-    if(err) return -1;
-    if(value3->value.i32)
+    unsigned int check_size = stack->entries->size;
+    if(check_size < 3) return -1;
+    stack->entries->pop(stack->entries, (void**)&value3);
+    stack->entries->pop(stack->entries, (void**)&value2);
+    stack->entries->pop(stack->entries, (void**)&value1);
+    if(value3->value.i32) {
         stack->entries->push(stack->entries, value1);
-    else
+        free_Value(value2);
+    } else {
         stack->entries->push(stack->entries, value2);
+        free_Value(value1);
+    }
+    free_Value(value3);
     return 0;
 }
