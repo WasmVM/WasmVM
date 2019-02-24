7aa9cb7d9b9ec47a1c91bf28d5bcbccc9d1f234a
YanJiun <jiun113241@gmail.com>
2019-02-24 19:42:59 +0800

    Implement i64_eqz instruction and unittest

--- src/lib/core/runtime/i64_eqz.c
+++ src/lib/core/runtime/i64_eqz.c
@@ -1,6 +1,12 @@
 #include <core/Runtime.h>
+#include <dataTypes/Value.h>
+#include <stddef.h>
 
 int runtime_i64_eqz(Stack* stack)
 {
+    Value *value1 = NULL;
+    if(stack->entries->pop(stack->entries, (void**)&value1)) return -1;
+    stack->entries->push(stack->entries, new_i32Value(value1->value.i64 == 0));
+    free_Value(value1);
     return 0;
 }
