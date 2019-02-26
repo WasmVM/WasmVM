d6dd24195c78f113f233772b13f5c15a8af7dff3
YanJiun <jiun113241@gmail.com>
2019-02-26 12:13:09 +0800

    Implement i64_const intruction and unittest

--- src/lib/core/runtime/i64_const.c
+++ src/lib/core/runtime/i64_const.c
@@ -1,6 +1,8 @@
 #include <core/Runtime.h>
+#include <dataTypes/Value.h>
 
 int runtime_i64_const(Stack* stack, int64_t value)
 {
+    stack->entries->push(stack->entries, new_i64Value(value));
     return 0;
 }
