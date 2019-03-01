ab2beb5b9adb644f4e470129f20d2b7a5581ef5a
YanJiun <jiun113241@gmail.com>
2019-03-01 23:56:11 +0800

    Implement i64_rem_s instruction and unittest

--- src/lib/core/runtime/i64_rem_s.c
+++ src/lib/core/runtime/i64_rem_s.c
@@ -1,6 +1,24 @@
 #include <core/Runtime.h>
+#include <dataTypes/Value.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 int runtime_i64_rem_s(Stack* stack)
 {
-    return 0;
+    Value *value1 = NULL, *value2 = NULL;
+    stack->entries->pop(stack->entries, (void**)&value2); // Divsor
+    stack->entries->pop(stack->entries, (void**)&value1); // Dividend
+
+    // Error detection: divided by zero
+    if (value2->value.i64 == 0) {
+        fprintf(stderr, "Divided by zero!\n");
+        free_Value(value1);
+        free_Value(value2);
+        return -1;
+    } else {
+        stack->entries->push(stack->entries, new_i64Value(value1->value.i64 % value2->value.i64));
+        free_Value(value1);
+        free_Value(value2);
+        return 0;
+    }
 }
