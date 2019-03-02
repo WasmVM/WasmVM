3996c4c160dda52ab57833a0500c03a8eb8471f5
YanJiun <jiun113241@gmail.com>
2019-03-02 18:53:32 +0800

    Implement i64_shl instruction and unittest

--- src/lib/core/runtime/i64_shl.c
+++ src/lib/core/runtime/i64_shl.c
@@ -1,6 +1,18 @@
 #include <core/Runtime.h>
+#include <dataTypes/Value.h>
+#include <stdlib.h>
 
 int runtime_i64_shl(Stack* stack)
 {
+    Value *value1 = NULL;
+    Value *value2 = NULL;
+
+    stack->entries->pop(stack->entries, (void **)&value2);
+    stack->entries->pop(stack->entries, (void **)&value1);
+
+    stack->entries->push(stack->entries, new_i64Value(value1->value.i64 << value2->value.i64));
+
+    free(value1);
+    free(value2);
     return 0;
 }
---
+++ test/unittests/lib/core/runtime/i64_shl_unittest.hpp
@@ -0,0 +1,29 @@
+#include <skypat/skypat.h>
+
+#define _Bool bool
+extern "C" {
+#include <dataTypes/Value.h>
+#include <core/Runtime.h>
+}
+#undef _Bool
+
+SKYPAT_F(Runtime_i64_shl, regular)
+{
+    // Prepare
+    Stack* stack = new_Stack((void (*)(void*))free_Value);
+    Value *value1 = new_i64Value(22L << 32), *value2 = new_i64Value(3);
+    stack->entries->push(stack->entries, value1);
+    stack->entries->push(stack->entries, value2);
+
+    // Run
+    runtime_i64_shl(stack);
+
+    // Check
+    Value *check = NULL;
+    stack->entries->pop(stack->entries, (void**)&check);
+    EXPECT_EQ(check->value.i64, 176L << 32);
+
+    // Clean
+    free_Value(check);
+    free_Stack(stack);
+}
