0eb7114b6047eb4aadbf0bd3b33dcb8b72f28e05
YanJiun <jiun113241@gmail.com>
2019-03-02 19:28:09 +0800

    Implement i64_shr_u instruction and unittest

--- src/lib/core/runtime/i64_shr_u.c
+++ src/lib/core/runtime/i64_shr_u.c
@@ -1,6 +1,18 @@
 #include <core/Runtime.h>
+#include <dataTypes/Value.h>
+#include <stdlib.h>
 
 int runtime_i64_shr_u(Stack* stack)
 {
+    Value *value1 = NULL;
+    Value *value2 = NULL;
+
+    stack->entries->pop(stack->entries, (void **)&value2);
+    stack->entries->pop(stack->entries, (void **)&value1);
+
+    stack->entries->push(stack->entries, new_i64Value(value1->value.u64 >> value2->value.u64));
+
+    free(value1);
+    free(value2);
     return 0;
 }
