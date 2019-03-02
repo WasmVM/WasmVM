ad76b9b22834b140fbedf0a4e0cf9969b260aea0
yuhao.kuo <st40201@gmail.com>
2019-03-02 21:16:13 +0800

    changed i32_shr_s bitwise operator Implementation

--- src/lib/core/runtime/i32_shr_s.c
+++ src/lib/core/runtime/i32_shr_s.c
@@ -6,20 +6,13 @@
 
 int runtime_i32_shr_s(Stack* stack)
 {
-    int32_t temp = 0;
     Value *value1 = NULL;
     Value *value2 = NULL;
 
     stack->entries->pop(stack->entries, (void **)&value2);
     stack->entries->pop(stack->entries, (void **)&value1);
 
-    temp = value1->value.i32;
-
-    temp = (temp >= 0)
-           ? temp >> value2->value.i32
-           : ~((~temp) >> value2->value.i32);
-
-    stack->entries->push(stack->entries, new_i32Value(temp));
+    stack->entries->push(stack->entries, new_i32Value(value1->value.i32 >> value2->value.i32));
 
     free(value1);
     free(value2);
