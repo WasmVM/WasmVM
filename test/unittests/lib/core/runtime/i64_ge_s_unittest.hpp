7c8daac68a6b32244939b960d4949106ea609678
YanJiun <jiun113241@gmail.com>
2019-03-02 23:38:03 +0800

    Implement i64_ge_s instruction and unittest

---
+++ test/unittests/lib/core/runtime/i64_ge_s_unittest.hpp
@@ -0,0 +1,30 @@
+#include <skypat/skypat.h>
+
+#define _Bool bool
+extern "C" {
+#include <core/Runtime.h>
+#include <dataTypes/Value.h>
+}
+#undef _Bool
+
+SKYPAT_F(Runtime_i64_ge_s, regular)
+{
+    // prepare
+    Stack* stack = new_Stack((void (*)(void*))free_Value);
+    Value *value1 = new_i64Value(-100L << 32);
+    Value *value2 = new_i64Value(10L << 32);
+    stack->entries->push(stack->entries, value1);
+    stack->entries->push(stack->entries, value2);
+
+    // run
+    runtime_i64_ge_s(stack);
+
+    // check
+    Value *check = NULL;
+    stack->entries->pop(stack->entries, (void**)&check);
+    EXPECT_EQ(check->value.i64, 0);
+
+    // clean
+    free_Value(check);
+    free_Stack(stack);
+}
