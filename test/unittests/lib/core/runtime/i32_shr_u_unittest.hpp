6f319e13e474c2addbb15f8187675dedfa8f09fd
yuhao.kuo <st40201@gmail.com>
2019-03-02 20:57:08 +0800

    fix i32_shr_u bitwise operator

--- test/unittests/lib/core/runtime/i32_shr_u_unittest.hpp
+++ test/unittests/lib/core/runtime/i32_shr_u_unittest.hpp
@@ -11,7 +11,7 @@ SKYPAT_F(Runtime_i32_shr_u, regular)
 {
     // Prepare
     Stack* stack = new_Stack((void (*)(void*))free_Value);
-    Value *value1 = new_i32Value(104), *value2 = new_i32Value(2);
+    Value *value1 = new_i32Value(0x90240059), *value2 = new_i32Value(2);
     stack->entries->push(stack->entries, value1);
     stack->entries->push(stack->entries, value2);
 
@@ -21,7 +21,7 @@ SKYPAT_F(Runtime_i32_shr_u, regular)
     // Check
     Value *check = NULL;
     stack->entries->pop(stack->entries, (void**)&check);
-    EXPECT_EQ(check->value.i32, 26);
+    EXPECT_EQ(check->value.i32, 0x24090016);
 
     // clean
     free_Value(check);
