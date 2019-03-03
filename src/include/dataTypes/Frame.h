5ecbe45c27368636d33dc3776e81274b25d66993
YangHau <vulxj0j8j8@gmail.com>
2019-03-03 20:27:04 +0800

    Implement new/delete function of Frame

--- src/include/dataTypes/Frame.h
+++ src/include/dataTypes/Frame.h
@@ -2,11 +2,15 @@
 #define WASMVM_INSTANCE_FRAME_DEF
 
 #include <dataTypes/vector.h>
-#include <ModuleInst.h>
+#include <instance/ModuleInst.h>
 
 typedef struct {
-    vector* vals;       // ValueType
-    ModuleInst module_inst;
+    vector* localVals;       // ValueType
+    ModuleInst* moduleInst;
 } Frame;
+
+Frame* new_Frame();
+void clean_Frame(Frame *thisFrame);
+void free_Frame(Frame *thisFrame);
 
 #endif
