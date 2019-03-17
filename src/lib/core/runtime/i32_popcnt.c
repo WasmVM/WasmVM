ca1f661dab5d813b3278b743fc90f5774c872a2d
LuisHsu <luishsuplus@gmail.com>
2019-03-17 21:44:55 +0800

    修正 i32_popcnt 的記憶體錯誤

--- src/lib/core/runtime/i32_popcnt.c
+++ src/lib/core/runtime/i32_popcnt.c
@@ -7,20 +7,20 @@ int runtime_i32_popcnt(Stack* stack)
     Value *value1 = NULL;
     pop_Value(stack,&value1);
 
-    uint32_t *n = &(value1->value.u32);
+    uint32_t n = value1->value.u32;
 
     // 11 (2) = 1 * 2 + 1 (10)
     // ab (2) = a * 2 + b (10)
     // ab >> 1 = a
     // ab - (ab >> 1) = a + b
-    *n -= (*n >> 1)  & 0x55555555;
-    *n  = (*n & 0x33333333) + ((*n >> 2) & 0x33333333);
-    *n  = ((*n >> 4) + *n) & 0x0F0F0F0F;
-    *n += *n >> 8;
-    *n += *n >> 16;
-    *n &= 0x0000003F;
+    n -= (n >> 1)  & 0x55555555;
+    n  = (n & 0x33333333) + ((n >> 2) & 0x33333333);
+    n  = ((n >> 4) + n) & 0x0F0F0F0F;
+    n += n >> 8;
+    n += n >> 16;
+    n &= 0x0000003F;
+    value1->value.u32 = n;
     push_Value(stack, value1);
-    free(value1);
 
     return 0;
 }
