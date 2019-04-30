824ca57eb2d699b8c641382c867af089d7518b8a
Kuo Yu-Hao <yuhao-kuo@users.noreply.github.com>
2019-04-30 19:14:55 +0800

    實作 Runtime 記憶體操作指令 (#206)
    * add f64 load runtime function and unittest
    
    * add f64 store runtime function and unittest

--- src/lib/core/runtime/f64_load.c
+++ src/lib/core/runtime/f64_load.c
@@ -1,6 +1,27 @@
 #include <core/Runtime.h>
+#include <dataTypes/Value.h>
+#include <instance/MemInst.h>
+#include <stdint.h>
+#include <stddef.h>
+#include <stdlib.h>
+#include <stdio.h>
 
 int runtime_f64_load(Stack* stack, MemInst* memory, uint32_t offset, uint32_t align)
 {
+    Value *value1 = NULL;
+
+    pop_Value(stack, &value1);
+    uint32_t ea = (uint32_t)value1->value.u32 + offset;
+
+    if (ea + (64 / 8) >= memory->max * 65536) {
+        fprintf(stderr, "over the memory range! \n");
+        free(value1);
+        return -1;
+    }
+
+    double *data = (double *) ((uint8_t *)  memory->data->data + ea);
+    push_Value(stack, new_f64Value(*data));
+    free(value1);
+
     return 0;
 }
--- src/lib/core/runtime/f64_store.c
+++ src/lib/core/runtime/f64_store.c
@@ -1,6 +1,37 @@
 #include <core/Runtime.h>
+#include <dataTypes/Value.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <stdio.h>
 
 int runtime_f64_store(Stack* stack, MemInst* memory, uint32_t offset, uint32_t align)
 {
+    Value *value1 = NULL;
+    Value *value2 = NULL;
+    double *dataPointer = NULL;
+    uint32_t ea = 0;
+
+    // pop data form stack
+    pop_Value(stack, &value2);
+    pop_Value(stack, &value1);
+
+    // check memory range
+    ea = (uint32_t) value1->value.u32 + offset;
+    if (ea + (64 / 8) >= memory->max * 65536) {
+        fprintf(stderr, "over the memory range! \n");
+        free_Value(value1);
+        free_Value(value2);
+        return -1;
+    }
+
+    // store data to memory
+    dataPointer = (double *) ((uint8_t *) memory->data->data + ea);
+    *dataPointer = value2->value.f64;
+
+    // free object
+    free(value1);
+    free(value2);
+
     return 0;
 }
