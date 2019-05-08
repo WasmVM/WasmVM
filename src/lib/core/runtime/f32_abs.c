617e6369fee499683736f119d0d0602047d2712d
Tim.Hsu <tim.hsu@vivotek.com>
2019-05-08 20:40:37 +0800

    Implement f32_abs instruction and unittest

--- src/lib/core/runtime/f32_abs.c
+++ src/lib/core/runtime/f32_abs.c
@@ -1,6 +1,13 @@
 #include <core/Runtime.h>
+#include <dataTypes/Value.h>
+#include <stdlib.h>
 
 int runtime_f32_abs(Stack* stack)
 {
+    Value* value1 = NULL;
+    stack->entries->pop(stack->entries, (void**)&value1);
+    value1->value.u32 &= 0x7fffffff;
+    value1->type = Value_f32;
+    stack->entries->push(stack->entries, value1);
     return 0;
 }
