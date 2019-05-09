a0d9b2460507624006cc3cfe47589bdeb843f4d8
Kuo Yu-Hao <yuhao-kuo@users.noreply.github.com>
2019-05-09 09:08:42 +0800

    實作 i64_store 記憶體操作指令及單元測試 (#213)
    * add i64_stor16 runtime function and unittest
    
    * add i64_store32 runtime function and unittest

--- src/lib/core/runtime/i64_store16.c
+++ src/lib/core/runtime/i64_store16.c
@@ -1,6 +1,37 @@
 #include <core/Runtime.h>
+#include <dataTypes/Value.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <stdio.h>
 
 int runtime_i64_store16(Stack* stack, MemInst* memory, uint32_t offset, uint32_t align)
 {
+    Value *value1 = NULL;
+    Value *value2 = NULL;
+    int16_t *dataPointer = NULL;
+    uint32_t ea = 0;
+
+    // pop data form stack
+    pop_Value(stack, &value2);
+    pop_Value(stack, &value1);
+
+    // check memory range
+    ea = (uint32_t) value1->value.u32 + offset;
+    if(ea + (16 / 8) >= memory->max * 65536) {
+        fprintf(stderr, "over the memory range! \n");
+        free_Value(value1);
+        free_Value(value2);
+        return -1;
+    }
+
+    // store data to memory
+    dataPointer = (int16_t *) ((int8_t *)memory->data->data + ea);
+    *dataPointer = (int16_t) value2->value.i64;
+
+    // free object
+    free(value1);
+    free(value2);
+
     return 0;
 }
---
+++ test/unittests/lib/core/runtime/i64_store16_unittest.hpp
@@ -0,0 +1,52 @@
+#include <skypat/skypat.h>
+
+#define _Bool bool
+extern "C" {
+#include <dataTypes/Value.h>
+#include <core/Runtime.h>
+#include <dataTypes/vector.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdint.h>
+}
+#undef _Bool
+
+SKYPAT_F(Runtime_i64_store16, regular)
+{
+    MemInst *memory = new_MemInst();
+    Stack   *stack  = new_Stack();
+    memory->max = 1;
+
+    uint32_t memlength = 16 * sizeof(int64_t);
+    uint32_t offset = 3 * sizeof(int64_t);
+    uint8_t  zero   = 0;
+
+    // init memory
+    for(uint32_t lop = 0; lop < memlength + offset; lop++) {
+        memory->data->push_back(memory->data, (void*) &zero);
+    }
+
+    // test store
+    for(uint32_t lop = 0; lop < 5; lop++) {
+        push_Value(stack, new_i32Value(lop * sizeof(int64_t)));
+        push_Value(stack, new_i64Value(lop + 1));
+
+        int ret = runtime_i64_store16(stack, memory, offset, 0);
+        EXPECT_EQ(ret, 0);
+
+        int16_t *d = (int16_t *)((int8_t *)memory->data->data + lop * sizeof(int64_t) + offset);
+        EXPECT_EQ(*d, (int16_t)(lop + 1));
+    }
+
+    // error check
+    Value *err      = new_i32Value(65537);
+    Value *err_data = new_i64Value(1);
+    push_Value(stack, err);
+    push_Value(stack, err_data);
+    int ret = runtime_i64_store16(stack, memory, offset, 0);  // ea > 65536 check
+    EXPECT_EQ(ret, -1);
+
+    free_MemInst(memory);
+    free_Stack(stack);
+}
