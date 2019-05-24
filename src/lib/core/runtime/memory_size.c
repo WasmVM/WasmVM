1c012de81bb97033a0dda223d99ca4a97ccf4ba6
YanJiun <jiun113241@gmail.com>
2019-05-24 08:30:48 +0800

    Repair redundant code

--- src/lib/core/runtime/memory_size.c
+++ src/lib/core/runtime/memory_size.c
@@ -8,6 +8,6 @@ int memory_size(Stack* stack, MemInst* memory)
     size_t vec_unitSize = memory->data->unitSize;
     uint32_t mem_data = vec_length * vec_unitSize;
     uint32_t pageSize = 64 * 1024;
-    push_Value(stack, new_i32Value((int32_t)(mem_data - 1) / pageSize + 1));
+    push_Value(stack, new_i32Value((int32_t)mem_data / pageSize ));
     return 0;
 }
