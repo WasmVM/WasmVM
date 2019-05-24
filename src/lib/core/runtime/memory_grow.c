c6bd366ec11df3b98ac4671b35690d94dbbeb198
YanJiun <jiun113241@gmail.com>
2019-05-24 16:55:33 +0800

    Formating Astyle.

--- src/lib/core/runtime/memory_grow.c
+++ src/lib/core/runtime/memory_grow.c
@@ -9,10 +9,10 @@ int memory_grow(Stack* stack, MemInst* memory)
     Value *value = NULL;
     const uint32_t pageSize = 64 * 1024;
     uint32_t sz = memory->data->length / pageSize;
-    
+
     // check stacktop value type i32
     stack->entries->top(stack->entries, (void**)&value);
-    if(value->type != Value_i32){
+    if(value->type != Value_i32) {
         fprintf(stderr, "memory grow by error value type!\n");
         return -1;
     }
@@ -22,12 +22,12 @@ int memory_grow(Stack* stack, MemInst* memory)
 
     // check memory max of pages and grow memory
     int32_t n = value->value.i32;
-    if(n > sz && n <= memory->max){
+    if(n > sz && n <= memory->max) {
         memory->data->resize(memory->data, n * pageSize);
     }
 
     // check result
-    if(memory->data->length != n * pageSize || memory->data == NULL){
+    if(memory->data->length != n * pageSize || memory->data == NULL) {
         push_Value(stack, new_i32Value(-1));
     } else {
         push_Value(stack, new_i32Value(sz));
