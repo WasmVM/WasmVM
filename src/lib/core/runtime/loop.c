cd40a3dd2e196b46d085e9ab82b6d0ac08a079ea
LuisHsu <luishsuplus@gmail.com>
2019-06-12 22:08:51 +0800

    修正 loop 的 contInstr

--- src/lib/core/runtime/loop.c
+++ src/lib/core/runtime/loop.c
@@ -6,7 +6,7 @@
 int runtime_loop(Stack stack, ControlInstrInst *control)
 {
     Label label = NULL;
-    label = new_Label(label_get_funcAddr(stack_cur_label(stack)), label_get_instrIndex(stack_cur_label(stack)) + 1, label_get_instrIndex(stack_cur_label(stack)) + 1);
+    label = new_Label(label_get_funcAddr(stack_cur_label(stack)), label_get_instrIndex(stack_cur_label(stack)) + 1, label_get_instrIndex(stack_cur_label(stack)));
     label_set_endInstr(label, control->endAddr);
     label_set_resultTypes(label, control->resultTypes);
     push_Label(stack, label);
