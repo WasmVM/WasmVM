757a7a5b9c457dde19a61940ddc53c9e875a5c7b
LuisHsu <luishsuplus@gmail.com>
2019-06-13 13:35:01 +0000

    實作 block 指令

---
+++ test/unittests/lib/core/runtime/block_unittest.hpp
@@ -0,0 +1,35 @@
+#include <skypat/skypat.h>
+
+#define _Bool bool
+extern "C" {
+#include <core/Runtime.h>
+#include <dataTypes/Label.h>
+#include <instance/ControlInstrInst.h>
+}
+#undef _Bool
+
+SKYPAT_F(Runtime_control_block, regular)
+{
+    Stack stack = new_Stack();
+
+    // Label: {Entry, funcAddr, instrIndex, contInstr}
+    Label currentLabel = new_Label(0, 1, 2);
+
+    ControlInstrInst* currentControl = new_ControlInstrInst();
+    currentControl->endAddr = 4;
+
+    push_Label(stack, currentLabel);
+
+    // Check
+    Label result = NULL;
+    runtime_block(stack, currentControl);
+    pop_Label(stack, &result);
+    EXPECT_EQ(label_get_instrIndex(result), 2);
+    EXPECT_EQ(label_get_funcAddr(result), 0);
+    EXPECT_EQ(label_get_contInstr(result), 4);
+    free(result);
+
+    // clean
+    free_Stack(stack);
+    free_ControlInstrInst(currentControl);
+}
