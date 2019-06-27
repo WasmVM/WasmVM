96bdd944856eaa05e60da52b52eb3608c7c0b63c
LuisHsu <luishsuplus@gmail.com>
2019-06-27 21:49:53 +0800

    實作 i32.trunc

--- src/lib/core/runtime/i32_trunc_s_f32.c
+++ src/lib/core/runtime/i32_trunc_s_f32.c
@@ -1,6 +1,12 @@
 #include <core/Runtime.h>
 
+#include <stdlib.h>
+
 int runtime_i32_trunc_s_f32(Stack stack)
 {
+    Value* operand = NULL;
+    pop_Value(stack, &operand);
+    push_Value(stack, new_i32Value((int32_t)operand->value.f32));
+    free(operand);
     return 0;
 }
