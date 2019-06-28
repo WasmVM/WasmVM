5ffb9d9428a7eed7e7043bad4790a78155330019
LuisHsu <luishsuplus@gmail.com>
2019-06-28 10:56:26 +0800

    實作 i64 trunc

---
+++ test/unittests/lib/core/runtime/i64_trunc_u_f64_unittest.hpp
@@ -0,0 +1,30 @@
+#include <skypat/skypat.h>
+
+#define _Bool bool
+extern "C" {
+#include <dataTypes/Value.h>
+#include <core/Runtime.h>
+}
+#undef _Bool
+
+SKYPAT_F(Runtime_i64_trunc_u_f64, regular)
+{
+    // Prepare
+    Stack stack = new_Stack();
+    Value *check = NULL;
+
+    push_Value(stack, new_f64Value(-22.5));
+    runtime_i64_trunc_u_f64(stack);
+    pop_Value(stack,&check);
+    EXPECT_EQ(check->value.i64, 22);
+    free_Value(check);
+
+    push_Value(stack, new_f64Value(22.5));
+    runtime_i64_trunc_u_f64(stack);
+    pop_Value(stack,&check);
+    EXPECT_EQ(check->value.i64, 22);
+    free_Value(check);
+
+    // Clean
+    free_Stack(stack);
+}
