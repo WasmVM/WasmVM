a0ac3ef6132f21d4b56c7071b60f5dc55594ac0b
LuisHsu <luishsuplus@gmail.com>
2019-06-30 19:56:33 +0800

    [WIP] 實作 f32 二元運算

--- src/lib/core/runtime/f32_eq.c
+++ src/lib/core/runtime/f32_eq.c
@@ -2,5 +2,15 @@
 
 int runtime_f32_eq(Stack stack)
 {
+    Value* operand1 = NULL;
+    Value* operand2 = NULL;
+
+    pop_Value(stack, &operand2);
+    pop_Value(stack, &operand1);
+
+    push_Value(stack, new_f32Value(operand1->value.f32 == operand2->value.f32));
+
+    free_Value(operand1);
+    free_Value(operand2);
     return 0;
 }
--- src/lib/core/runtime/f32_min.c
+++ src/lib/core/runtime/f32_min.c
@@ -1,6 +1,18 @@
 #include <core/Runtime.h>
 
+#include <math.h>
+
 int runtime_f32_min(Stack stack)
 {
+    Value* operand1 = NULL;
+    Value* operand2 = NULL;
+
+    pop_Value(stack, &operand2);
+    pop_Value(stack, &operand1);
+
+    push_Value(stack, new_f32Value(fminf(operand1->value.f32, operand2->value.f32)));
+
+    free_Value(operand1);
+    free_Value(operand2);
     return 0;
 }
