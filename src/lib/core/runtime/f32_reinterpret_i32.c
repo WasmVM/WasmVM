814af9da279dafa34f70e744447041ad0bf37d05
LuisHsu <luishsuplus@gmail.com>
2019-06-30 15:41:14 +0800

    實作 reinterpret

--- src/lib/core/runtime/f32_reinterpret_i32.c
+++ src/lib/core/runtime/f32_reinterpret_i32.c
@@ -1,6 +1,12 @@
 #include <core/Runtime.h>
 
+#include <stdlib.h>
+
 int runtime_f32_reinterpret_i32(Stack stack)
 {
+    Value* operand = NULL;
+    pop_Value(stack, &operand);
+    push_Value(stack, new_f32Value(operand->value.f32));
+    free(operand);
     return 0;
 }
