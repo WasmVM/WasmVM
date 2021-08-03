2c2051a51fc36a3c87ffdf5f1c5880b81e463bed
LuisHsu <luishsuplus@gmail.com>
2021-08-03 06:34:04 +0000

    Define WasmVM API and basic scalar types

---
+++ src/include/WasmVM.h
@@ -0,0 +1,31 @@
+#ifndef WASMVM_DEF
+#define WASMVM_DEF
+
+// TODO: store_init()
+// TODO: module_decode()
+// TODO: module_validate()
+// TODO: module_instantiate()
+// TODO: module_imports()
+// TODO: module_exports()
+// TODO: instance_export()
+// TODO: func_alloc()
+// TODO: func_type()
+// TODO: func_invoke()
+// TODO: table_alloc()
+// TODO: table_type()
+// TODO: table_read()
+// TODO: table_write()
+// TODO: table_size()
+// TODO: table_grow()
+// TODO: mem_alloc()
+// TODO: mem_type()
+// TODO: mem_read()
+// TODO: mem_write()
+// TODO: mem_size()
+// TODO: mem_grow()
+// TODO: global_alloc()
+// TODO: global_type()
+// TODO: global_read()
+// TODO: global_write()
+
+#endif
