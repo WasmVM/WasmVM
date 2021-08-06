93fbe554078870ae8cffca53927f7c5376e70c8d
LuisHsu <luishsuplus@gmail.com>
2021-08-06 08:55:34 +0000

    Implement load_file for test helper

---
+++ test/harness/helper.c
@@ -0,0 +1,35 @@
+#include <helper.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+
+char* load_file(const char* filename, long* size)
+{
+    FILE* fin = NULL;
+    // Open file
+    if((fin = fopen(filename, "rb")) == NULL) {
+        return NULL;
+    }
+    // Get file size
+    if(fseek(fin, 0L, SEEK_END)) {
+        return NULL;
+    }
+    *size = ftell(fin);
+    if(*size == -1L) {
+        return NULL;
+    }
+    if(fseek(fin, 0L, SEEK_SET)) {
+        return NULL;
+    }
+    // Allocate data memory
+    char* data = malloc(*size);
+    if(data == NULL) {
+        return NULL;
+    }
+    // Read file
+    if(fread(data, sizeof(char), *size, fin) != *size) {
+        return NULL;
+    }
+    fclose(fin);
+    return data;
+}
---
+++ test/harness/helper.h
@@ -0,0 +1,6 @@
+#ifndef WASMVM_TEST_HELPER_DEF
+#define WASMVM_TEST_HELPER_DEF
+
+char* load_file(const char* filename, long* size);
+
+#endif
