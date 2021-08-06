a25f97b036130eacf01b73eef338e2ea037517b8
LuisHsu <luishsuplus@gmail.com>
2021-08-06 10:03:08 +0000

    Exclude third_party from style check

--- .vscode/astyle.sh
+++ .vscode/astyle.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-astyle --style=kr --exclude=build --indent=spaces=4 --indent-switches --suffix=none --recursive *.c *.h *.hpp
+astyle --style=kr --exclude=build*/ --exclude=third_party/ --indent=spaces=4 --indent-switches --suffix=none --recursive *.c *.h *.hpp
