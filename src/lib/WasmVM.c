eef089ad3caf137053c09f861d5feb35dfc42377
LuisHsu <luishsuplus@gmail.com>
2021-08-06 10:03:32 +0000

    Define decode.c

--- src/lib/WasmVM.c
+++ src/lib/WasmVM.c
@@ -1,12 +1,12 @@
+/**
+ * Copyright 2021 Luis Hsu, Kevin Cyu. All rights reserved.
+ * Use of this source code is governed by a BSD-style
+ * license that can be found in the LICENSE file.
+ */
+
 #include <WasmVM.h>
-
-int module_decode(char* data, wasm_module* module)
-{
-
-    return 0;
-}
 
 void module_free(wasm_module module)
 {
-
+    // TODO:
 }
---
+++ src/lib/decode/decode.c
@@ -0,0 +1,23 @@
+/**
+ * Copyright 2021 Luis Hsu, Kevin Cyu. All rights reserved.
+ * Use of this source code is governed by a BSD-style
+ * license that can be found in the LICENSE file.
+ */
+
+#include <WasmVM.h>
+#include <defines.h>
+#include <structures/WasmModule.h>
+
+int module_decode(const char* data, const size_t data_size, WasmModule** module)
+{
+    // Allocate module memory
+    if((*module = (WasmModule*)malloc_func(sizeof(WasmModule))) == NULL) {
+        return -1;
+    }
+
+    // Cursor pointers
+    const char* cursor_p = data;
+    const char* end_p = data + data_size - 1;
+
+    return 0;
+}
