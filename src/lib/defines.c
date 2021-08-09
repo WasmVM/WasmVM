de22f52136239f8113016cb3753db98ff7354def
LuisHsu <luishsuplus@gmail.com>
2021-08-09 23:54:47 +0000

    Implement memcpy

---
+++ src/lib/defines.c
@@ -0,0 +1,14 @@
+/**
+ * Copyright 2021 Luis Hsu, Kevin Cyu. All rights reserved.
+ * Use of this source code is governed by a BSD-style
+ * license that can be found in the LICENSE file.
+ */
+
+#include <defines.h>
+
+void memcpy_func(char** const dst, const char* const src, u64_t size)
+{
+    for(u64_t i = 0; i < size; ++i) {
+        (*dst)[i] = src[i];
+    }
+}
