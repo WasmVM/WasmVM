68889b002bd49cfa3fc84a4b8606d1dd009fb325
LuisHsu <luishsuplus@gmail.com>
2021-10-12 23:28:31 +0000

    Define TableInst & MemInst in Store

--- src/include/Store.h
+++ src/include/Store.h
@@ -4,11 +4,12 @@
 #include <dataTypes/vector_t.h>
 #include <instance/FuncInst.h>
 #include <instance/TableInst.h>
+#include <instance/MemInst.h>
 
 typedef struct {
-    vector_t(FuncInst) funcs;      // FuncInst
-    vector_t(TableInst) tables;     // TableInst
-    // vector_t(FuncInst) mems;       // MemInst
+    vector_t(FuncInst) funcs;
+    vector_t(TableInst) tables;
+    vector_t(MemInst) mems;
     // vector_t(FuncInst) globals;    // GlobalInst
 } Store;
 
--- src/include/instance/TableInst.h
+++ src/include/instance/TableInst.h
@@ -6,7 +6,6 @@
 #include <dataTypes/RefType.h>
 
 typedef struct {
-    u32_t min;
     u32_t max;
     RefType type;
     vector_t(Ref) elem; // uint32_t
