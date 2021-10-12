98ac1fae9d0f109ca93d6e27db326ca04e77a4cc
LuisHsu <luishsuplus@gmail.com>
2021-10-12 22:58:43 +0000

    Define TableInst

--- src/include/Store.h
+++ src/include/Store.h
@@ -3,10 +3,11 @@
 
 #include <dataTypes/vector_t.h>
 #include <instance/FuncInst.h>
+#include <instance/TableInst.h>
 
 typedef struct {
     vector_t(FuncInst) funcs;      // FuncInst
-    // vector_t(TableInst) tables;     // TableInst
+    vector_t(TableInst) tables;     // TableInst
     // vector_t(FuncInst) mems;       // MemInst
     // vector_t(FuncInst) globals;    // GlobalInst
 } Store;
