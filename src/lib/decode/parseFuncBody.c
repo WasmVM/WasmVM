000be1d10ddb74f4eff465a8a45d3bf90cb49738
LuisHsu <luishsuplus@gmail.com>
2021-10-14 07:43:59 +0000

    Update license declaration

--- src/lib/decode/parseFuncBody.c
+++ src/lib/decode/parseFuncBody.c
@@ -1,3 +1,8 @@
+/**
+ * Copyright 2021 Luis Hsu, Kevin Cyu. All rights reserved.
+ * Use of this source code is governed by a BSD-style
+ * license that can be found in the LICENSE file.
+ */
 #include "parseFuncBody.h"
 
 #include <defines.h>
--- src/lib/store.c
+++ src/lib/store.c
@@ -1,5 +1,10 @@
-#include <WasmVM.h>
+/**
+ * Copyright 2021 Luis Hsu, Kevin Cyu. All rights reserved.
+ * Use of this source code is governed by a BSD-style
+ * license that can be found in the LICENSE file.
+ */
 
+#include <WasmVM.h>
 #include <defines.h>
 
 Store* store_init()
