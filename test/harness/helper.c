811fe5f75d4148e77ce4ce69b1bfec85ce831942
LuisHsu <luishsuplus@gmail.com>
2021-11-04 07:24:48 +0000

    Implement import matching

--- test/harness/helper.c
+++ test/harness/helper.c
@@ -1,7 +1,13 @@
+/**
+ * Copyright 2021 Luis Hsu. All rights reserved.
+ * Use of this source code is governed by a BSD-style
+ * license that can be found in the LICENSE file.
+ */
 #include <helper.h>
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <error.h>
 
 byte_t* load_file(const char* filename, size_t* size)
 {
@@ -32,4 +38,75 @@ byte_t* load_file(const char* filename, size_t* size)
     }
     fclose(fin);
     return data;
+}
+
+static _Bool name_compare(
+    const size_t size1, const byte_t name1[size1],
+    const size_t size2, const byte_t name2[size2]
+)
+{
+    if(size1 != size2) {
+        return 0;
+    }
+    for(size_t i = 0; i < size1; ++i) {
+        if(name1[i] != name2[i]) {
+            return 0;
+        }
+    }
+    return 1;
+}
+
+int match_imports(const wasm_module module, const struct _hashmap* moduleInsts, size_t* size, ExternVal** externvals)
+{
+    // Get imports
+    ExternType* imports = NULL;
+    if(module_imports(module, size, &imports)) {
+        *externvals = NULL;
+        *size = 0;
+        return -1;
+    }
+    // Allocate externVals
+    *externvals = (ExternVal*) malloc_func(sizeof(ExternVal) * (*size));
+    // Retrieve externvals
+    for(size_t impIdx = 0; impIdx < *size; ++impIdx) {
+        // Get module_inst
+        wasm_module_inst moduleInst = hashmap_get(
+                                          ModuleInst,
+                                          sizeof(byte_t) * imports[impIdx].module.size,
+                                          imports[impIdx].module.data,
+                                          moduleInsts
+                                      );
+        if(moduleInst == NULL) {
+            // Module not found
+            free(*externvals);
+            *externvals = NULL;
+            *size = 0;
+            wasmvm_errno = ERROR_unknown_import;
+            return -1;
+        }
+        // Get export
+        ExternVal* exportValPtr = NULL;
+        for(unsigned int expIdx = 0; expIdx < moduleInst->exports.size; ++expIdx) {
+            if(name_compare(
+                        moduleInst->exports.data[expIdx].name.size,
+                        moduleInst->exports.data[expIdx].name.data,
+                        imports[impIdx].name.size,
+                        imports[impIdx].name.data
+                    )) {
+                exportValPtr = &(moduleInst->exports.data[expIdx].value);
+                break;
+            }
+        }
+        if(exportValPtr == NULL) {
+            // Export not found
+            free(*externvals);
+            *externvals = NULL;
+            *size = 0;
+            wasmvm_errno = ERROR_unknown_import;
+            return -1;
+        } else {
+            (*externvals)[impIdx] = *exportValPtr;
+        }
+    }
+    return 0;
 }
--- test/harness/helper.h
+++ test/harness/helper.h
@@ -2,7 +2,10 @@
 #define WASMVM_TEST_HELPER_DEF
 
 #include <defines.h>
+#include <WasmVM.h>
+#include <hashmap_t.h>
 
 byte_t* load_file(const char* filename, size_t* size);
+int match_imports(const wasm_module module, const struct _hashmap* moduleInsts, size_t* size, ExternVal** externvals);
 
 #endif
