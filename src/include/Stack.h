d77ce0ca5d7779585cf3c9aa0ec24861d29617d8
LuisHsu <luishsuplus@gmail.com>
2022-03-29 06:22:08 +0800

    Define Stack

--- src/include/Stack.h
+++ src/include/Stack.h
@@ -1,10 +1,38 @@
 #ifndef WASMVM_STACK_DEF
 #define WASMVM_STACK_DEF
 
-typedef struct {
-    // TODO:
-} Stack;
+#include <defines.h>
+#include <dataTypes/vector_t.h>
+#include <dataTypes/Value.h>
+#include <instances/ModuleInst.h>
+#include <instances/InstrInst.h>
 
-typedef Stack* wasm_stack;
+typedef struct {
+    u32_t arity;
+    vector_t(Value) locals;
+    wasm_module_inst moduleinst;
+} Frame;
+
+typedef struct {
+    u32_t arity;
+    InstrInst* current, end;
+} Label;
+
+typedef enum {
+    Entry_unspecified,
+    Entry_frame,
+    Entry_label,
+    Entry_value,
+} EntryType;
+
+typedef struct _Stack{
+    EntryType type;
+    union {
+        Frame frame;
+        Label label;
+        Value value;
+    } entry;
+    struct _Stack* next;
+} Stack;
 
 #endif
