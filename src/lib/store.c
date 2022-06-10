31234cfe5262ab62f58c3b5c5a0b40d4effe82af
LuisHsu <luishsuplus@gmail.com>
2022-06-10 01:12:07 -0700

    Free funcinst & meminst

--- src/lib/store.c
+++ src/lib/store.c
@@ -19,23 +19,38 @@ Store* store_init()
     return store;
 }
 
+static void free_FuncInst(FuncInst func){
+    if(func.bodyType == FuncBody_Wasm){
+        free_vector(func.body.wasm.locals);
+        free_vector(func.body.wasm.codes);
+    }
+}
+
 void store_free(Store* store)
 {
     if(store != NULL) {
-        free_vector(store->funcs);        // FIXME:
+        // Funcs
+        for(u32_t i = 0; i < store->funcs.size; ++i){
+            free_FuncInst(store->funcs.data[i]);
+        }
+        free_vector(store->funcs);
         // Tables
         for(u32_t i = 0; i < store->tables.size; ++i){
             free_vector(store->tables.data[i].elem);
         }
-        free_vector(store->tables);        // FIXME:
-        free_vector(store->mems);        // FIXME:
+        free_vector(store->tables);
+        // Mems
+        for(u32_t i = 0; i < store->mems.size; ++i){
+            free_vector(store->mems.data[i].data);
+        }
+        free_vector(store->mems);
         free_vector(store->globals);        // FIXME:
         free_vector(store->elems);        // FIXME:
         // Datas
         for(u32_t i = 0; i < store->datas.size; ++i){
             free_vector(store->datas.data[i].data);
         }
-        free_vector(store->datas);        // FIXME:
+        free_vector(store->datas);
         // Store
         free_func(store);
     }
