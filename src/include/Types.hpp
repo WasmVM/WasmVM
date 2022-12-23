53dcd2f4b0b797563facbac302c91e03607b287b
LuisHsu <luishsuplus@gmail.com>
2022-12-23 16:59:08 -0600

    Define C++ basic types

--- src/include/Types.hpp
+++ src/include/Types.hpp
@@ -1,13 +1,23 @@
 #ifndef WASMVM_PP_DATATYPES_VALUE_DEF
 #define WASMVM_PP_DATATYPES_VALUE_DEF
 
+#include <vector>
 #include <variant>
 #include <any>
+
+namespace WasmVM {
 
 enum class ValueType {
     i32, i64, f32, f64, ref
 };
 
 using Value = std::variant<int32_t, int64_t, float, double, std::any>;
+
+struct FuncType {
+    std::vector<ValueType> params;
+    std::vector<ValueType> results;
+};
+
+}
 
 #endif
