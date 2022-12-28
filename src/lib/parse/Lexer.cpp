f6876c5f616ce26839d1c2ca0f46d0a574504fe5
LuisHsu <luishsuplus@gmail.com>
2022-12-28 01:47:08 -0500

    [WIP] Implement SourceIter to iterate source file

--- src/lib/parse/Lexer.cpp
+++ src/lib/parse/Lexer.cpp
@@ -5,153 +5,260 @@
 #include "Lexer.hpp"
 
 #include <exception.hpp>
+#include <cassert>
 
 using namespace WasmVM;
 
-Stream::Stream(std::istream& stream) : stream(stream){}
+SourceIter::SourceIter(std::istream& stream) : stream(stream), pos(0), lineno(1), colno(0), newline(false){
+    advance();
+}
 
-static char _get(std::stack<char> &buf, std::istream &stream){
-    if(buf.empty()){
-        return stream.get();
-    }else{
-        char ch = buf.top();
+SourceIter::reference SourceIter::operator*() const {
+    if(pos == end_pos){
+        return eof;
+    }
+    return buf.front().first;
+}
+
+SourceIter::operator bool(){
+    return pos != end_pos;
+}
+
+SourceIter& SourceIter::operator++(){
+    if(pos != end_pos){
+        pos += 1;
         buf.pop();
-        return ch;
+        if(buf.empty()){
+            advance();
+        }
     }
+    return *this;
 }
 
-static char _peek(std::stack<char> &buf, std::istream &stream){
-    if(buf.empty()){
-        return stream.peek();
-    }else{  
-        return buf.top();
-    }
+std::pair<size_t, size_t>& SourceIter::location(){
+    return buf.front().second;
 }
 
-static bool _eof(std::stack<char> &buf, std::istream &stream) {
-    return buf.empty() && stream.eof();
-}
-
-static void skip_comment(std::stack<char> &buf, std::istream &stream){
-    char ch;
-    while(true){
-        switch(_peek(buf, stream)){
-            // Line comment
-            case ';':
-                ch = _get(buf, stream);
-                if(_peek(buf, stream) == ';'){
-                    while(!_eof(buf, stream) && ((ch = _get(buf, stream)) != '\n'));
-                    buf.push(ch);
-                }else{
-                    buf.push(ch);
-                    return;
-                }
-            break;
+void SourceIter::advance(){
+    std::istream& str = stream.get();
+    for (traits_type::int_type ch = get(); ch != traits_type::eof(); ){
+        switch(ch){
             // Block comment
             case '(':
-                ch = _get(buf, stream);
-                if(_peek(buf, stream) == ';'){
-                    ssize_t depth = 1;
-                    ch = _get(buf, stream);
-                    while(!_eof(buf, stream)){
-                        ch = _get(buf, stream);
-                        char pe = _peek(buf, stream);
+                if(str.peek() == ';'){
+                    get();
+                    size_t depth = 1;
+                    for(ch = get(); (depth > 0) && (ch != traits_type::eof()); ch = get()){
+                        char pe = str.peek();
                         if((ch == '(') && (pe == ';')){
                             depth += 1;
-                            ch = _get(buf, stream);
+                            get();
                         }else if((ch == ';') && (pe == ')')){
                             depth -= 1;
-                            ch = _get(buf, stream);
-                            if(depth == 0){
-                                return;
-                            }
+                            get();
                         }
                     }
-                    throw WasmVM::exception::eof("block comment reach EOF without close");
                 }else{
-                    buf.push(ch);
-                    return;
+                    buf.emplace(ch, std::pair<size_t, size_t>{lineno, colno});
+                    ch = get();
+                }
+            break;
+            // Line comment
+            case ';':
+                if(str.peek() == ';'){
+                    get();
+                    for(ch = get(); (ch != eof) && (ch != '\n'); ch = get());
                 }
             break;
             // Skip extra spaces
             case ' ':
             case '\n':
             case '\t':
-            case '\r':{
-                ch = _get(buf, stream);
-                char pe = _peek(buf, stream);
-                if(pe == ';'){
-                    break;
-                }
-                if(_eof(buf, stream) || ((pe != ' ') && (pe != '\t') && (pe != '\n') && (pe != '\r'))){
-                    buf.push((ch == '\n') ? ' ' : ch);
-                    return;
-                }
-                for(; !_eof(buf, stream) && ((pe == ' ') || (pe == '\t') || (pe == '\n') || (pe == '\r')); ch = _get(buf, stream), pe = _peek(buf, stream));
-                buf.push((ch == '\n') ? ' ' : ch);
-            }break;
-            default:
-            return;
-        }
-    }
-}
-
-char Stream::get(){
-    skip_comment(buf, stream);
-    return _get(buf, stream);
-}
-
-char Stream::peek(){
-    skip_comment(buf, stream);
-    return _peek(buf, stream);
-}
-
-Slicer::Slicer(std::istream& stream) : Stream(stream){
-    operator++();
-}
-
-const std::string& Slicer::operator*() const{
-    return current;
-}
-
-Slicer::operator bool(){
-    return !current.empty();
-}
-
-Slicer& Slicer::operator++(){
-    static const int eof_char = std::char_traits<char>::eof();
-    current.clear();
-    char ch = get();
-    // Trim leading spaces
-    while((ch == ' ') || (ch == '\t') || (ch == '\r') || (ch == '\n')){
-        ch = get();
-    }
-    // Parenthesis
-    if((ch == '(') || (ch == ')')){
-        current = ch;
-        return *this;
-    }
-    // Other characters
-    while(ch != eof_char){
-        switch(ch){
-            case ' ':
-            case '\t':
             case '\r':
-            case '\n':
-            case '(':
-            case ')':
-                return *this;
-            default:
-                current += ch;
+                buf.emplace(' ', std::pair<size_t, size_t>{lineno, colno});
+                do{
+                    ch = get();
+                }while((ch == ' ') || (ch == '\n') || (ch == '\t') || (ch == '\r'));
             break;
+            default:
+                buf.emplace(ch, std::pair<size_t, size_t>{lineno, colno});
+                return;
         }
-        ch = get();
     }
-    return *this;
+    pos = end_pos;
 }
 
-Slicer Slicer::operator++(int){
-    Slicer res = *this;
-    ++(*this);
-    return res;
+bool SourceIter::operator==(const SourceIter& it) const {
+    return pos == it.pos;
 }
+bool SourceIter::operator!=(const SourceIter& it) const {
+    return pos != it.pos;
+}
+
+SourceIter::traits_type::int_type SourceIter::get(){
+    SourceIter::traits_type::int_type ch = stream.get().get();
+    if(newline){
+        lineno += 1;
+        colno = 0;
+        newline = false;
+    }
+    if(ch == '\n'){
+        newline = true;
+    }
+    if(ch != eof){
+        colno += 1;
+    }
+    return ch;
+}
+
+// SourceIter operator++(int);
+
+// static void _skip_comment(std::stack<char> &buf, std::istream &stream){
+//     char ch;
+//     while(true){
+//         switch(_peek(buf, stream)){
+//             // Block comment
+//             case '(':
+//                 ch = _get(buf, stream);
+//                 if(_peek(buf, stream) == ';'){
+//                     ssize_t depth = 1;
+//                     ch = _get(buf, stream);
+//                     while(!_eof(buf, stream)){
+//                         ch = _get(buf, stream);
+//                         char pe = _peek(buf, stream);
+//                         if((ch == '(') && (pe == ';')){
+//                             depth += 1;
+//                             ch = _get(buf, stream);
+//                         }else if((ch == ';') && (pe == ')')){
+//                             depth -= 1;
+//                             ch = _get(buf, stream);
+//                             if(depth == 0){
+//                                 return;
+//                             }
+//                         }
+//                     }
+//                     throw WasmVM::exception::eof("block comment reach EOF without close");
+//                 }else{
+//                     buf.push(ch);
+//                     return;
+//                 }
+//             break;
+//             // Skip extra spaces
+//             case ' ':
+//             case '\n':
+//             case '\t':
+//             case '\r':{
+//                 ch = _get(buf, stream);
+//                 char pe = _peek(buf, stream);
+//                 if(pe == ';'){
+//                     break;
+//                 }
+//                 if(_eof(buf, stream) || ((pe != ' ') && (pe != '\t') && (pe != '\n') && (pe != '\r'))){
+//                     buf.push((ch == '\n') ? ' ' : ch);
+//                     return;
+//                 }
+//                 for(; !_eof(buf, stream) && ((pe == ' ') || (pe == '\t') || (pe == '\n') || (pe == '\r')); ch = _get(buf, stream), pe = _peek(buf, stream));
+//                 buf.push((ch == '\n') ? ' ' : ch);
+//             }break;
+//             default:
+//             return;
+//         }
+//     }
+// }
+
+// char LexerStream::get(bool skip_comment){
+//     if(skip_comment){
+//         _skip_comment(buf, stream);
+//     }
+//     int res = _get(buf, stream);
+//     if(res != std::char_traits<std::istream>::eof()){
+//         pos += 1;
+//     }
+//     return res;
+// }
+
+// char LexerStream::peek(bool skip_comment){
+//     if(skip_comment){
+//         _skip_comment(buf, stream);
+//     }
+//     return _peek(buf, stream);
+// }
+
+// void LexerStream::put(char ch){
+//     buf.push(ch);
+//     pos -= 1;
+// }
+
+// std::ptrdiff_t LexerStream::seek() const{
+//     return pos;
+// }
+
+// LexerSlicer::LexerSlicer(std::istream& stream) : LexerStream(stream){
+//     next();
+// }
+
+// const std::string& LexerSlicer::operator*() const{
+//     return current;
+// }
+
+// LexerSlicer::operator bool(){
+//     return !current.empty();
+// }
+
+// const std::string& LexerSlicer::operator++(){
+//     static const int eof_char = std::char_traits<char>::eof();
+//     current.clear();
+//     char ch = get();
+//     // Trim leading spaces
+//     while((ch == ' ') || (ch == '\t') || (ch == '\r') || (ch == '\n')){
+//         ch = get();
+//     }
+//     // Parenthesis
+//     if((ch == '(') || (ch == ')')){
+//         current = ch;
+//         return current;
+//     }
+//     // Other characters
+//     while(ch != eof_char){
+//         switch(ch){
+//             case '(':
+//             case ')':
+//                 put(ch);
+//             case ' ':
+//             case '\t':
+//             case '\r':
+//             case '\n':
+//                 return current;
+//             default:
+//                 current += ch;
+//             break;
+//         }
+//         ch = get();
+//     }
+//     return current;
+// }
+
+// LexerSlicer& LexerSlicer::next(void){
+//     ++(*this);
+//     return *this;
+// }
+
+// std::string LexerSlicer::operator++(int){
+//     std::string res = current;
+//     next();
+//     return res;
+// }
+
+// Lexer::Lexer(std::istream& stream) : LexerSlicer(stream){}
+
+// std::optional<std::reference_wrapper<Token>> Lexer::operator[](size_t index) {
+//     if(tokens.size() <= index){
+//         for()
+//     }else{
+//         return tokens[index];
+//     }
+// }
+
+// std::vector<Token> Lexer::consume(size_t index) {
+//     return std::vector<Token>();
+// }
--- src/lib/parse/Lexer.hpp
+++ src/lib/parse/Lexer.hpp
@@ -2,37 +2,69 @@
 #define WASMVM_PP_PARSE_Slicer_DEF
 
 #include <istream>
-#include <stack>
+#include <queue>
 #include <string>
+#include <vector>
+#include <iterator>
 
 #include "Token.hpp"
 
 namespace WasmVM {
 
-struct Stream {
+struct SourceIter {
 
-    Stream(std::istream& stream);
+    using traits_type = std::char_traits<char>;
+    using difference_type = traits_type::off_type;
+    using value_type = traits_type::char_type;
+    using pointer = void;
+    using reference = const traits_type::char_type&;
+    using iterator_category = std::input_iterator_tag;
+    constexpr static value_type eof = traits_type::eof();
+    
+    SourceIter(std::istream& stream);
 
-    char get();
-    char peek();
-
-private:
-    std::istream& stream;
-    std::stack<char> buf;
-};
-
-struct Slicer : public Stream{
-
-    Slicer(std::istream& stream);
-
-    const std::string& operator*() const;
-    Slicer& operator++();
-    Slicer operator++(int);
+    reference operator*() const;
+    SourceIter& operator++();
+    SourceIter operator++(int);
+    inline bool operator==(const SourceIter&) const;
+    inline bool operator!=(const SourceIter&) const;
     operator bool();
+    std::pair<size_t, size_t>& location();
 
 private:
-    std::string current;
+    std::reference_wrapper<std::istream> stream;
+    constexpr static difference_type end_pos = -1;
+    difference_type pos;
+    std::queue<std::pair<value_type, std::pair<size_t, size_t>>> buf;
+    void advance();
+    size_t lineno, colno;
+    traits_type::int_type get();
+    bool newline;
 };
+static_assert(std::input_iterator<SourceIter>);
+
+// struct LexerSlicer : public LexerStream {
+
+//     using difference_type = std::ptrdiff_t;
+
+//     LexerSlicer(std::istream& stream);
+
+// private:
+
+//     std::string current;
+// };
+// static_assert(std::forward_iterator<LexerSlicer>);
+
+// struct Lexer : protected LexerSlicer {
+
+//     Lexer(std::istream& stream);
+
+
+//     std::vector<Token> consume(size_t index = 0);
+
+// private:
+//     std::vector<Token> tokens;
+// };
 
 }
 
