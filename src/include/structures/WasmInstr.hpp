6691a8dd90a52cbc715686505142b3acc9b3c67f
LuisHsu <luishsuplus@gmail.com>
2023-03-16 09:14:30 +0000

    Define numeric instruction structures

--- src/include/structures/WasmInstr.hpp
+++ src/include/structures/WasmInstr.hpp
@@ -177,6 +177,142 @@ struct F64_const : public Base {
     F64_const(f64_t value): Base(Opcode::F64_const), value(value) {}
     f64_t value;
 };
+using I32_eqz = Atomic<Opcode::I32_eqz>;
+using I32_eq = Atomic<Opcode::I32_eq>;
+using I32_ne = Atomic<Opcode::I32_ne>;
+using I32_lt_s = Atomic<Opcode::I32_lt_s>;
+using I32_lt_u = Atomic<Opcode::I32_lt_u>;
+using I32_gt_s = Atomic<Opcode::I32_gt_s>;
+using I32_gt_u = Atomic<Opcode::I32_gt_u>;
+using I32_le_s = Atomic<Opcode::I32_le_s>;
+using I32_le_u = Atomic<Opcode::I32_le_u>;
+using I32_ge_s = Atomic<Opcode::I32_ge_s>;
+using I32_ge_u = Atomic<Opcode::I32_ge_u>;
+using I64_eqz = Atomic<Opcode::I64_eqz>;
+using I64_eq = Atomic<Opcode::I64_eq>;
+using I64_ne = Atomic<Opcode::I64_ne>;
+using I64_lt_s = Atomic<Opcode::I64_lt_s>;
+using I64_lt_u = Atomic<Opcode::I64_lt_u>;
+using I64_gt_s = Atomic<Opcode::I64_gt_s>;
+using I64_gt_u = Atomic<Opcode::I64_gt_u>;
+using I64_le_s = Atomic<Opcode::I64_le_s>;
+using I64_le_u = Atomic<Opcode::I64_le_u>;
+using I64_ge_s = Atomic<Opcode::I64_ge_s>;
+using I64_ge_u = Atomic<Opcode::I64_ge_u>;
+using F32_eq = Atomic<Opcode::F32_eq>;
+using F32_ne = Atomic<Opcode::F32_ne>;
+using F32_lt = Atomic<Opcode::F32_lt>;
+using F32_gt = Atomic<Opcode::F32_gt>;
+using F32_le = Atomic<Opcode::F32_le>;
+using F32_ge = Atomic<Opcode::F32_ge>;
+using F64_eq = Atomic<Opcode::F64_eq>;
+using F64_ne = Atomic<Opcode::F64_ne>;
+using F64_lt = Atomic<Opcode::F64_lt>;
+using F64_gt = Atomic<Opcode::F64_gt>;
+using F64_le = Atomic<Opcode::F64_le>;
+using F64_ge = Atomic<Opcode::F64_ge>;
+using I32_clz = Atomic<Opcode::I32_clz>;
+using I32_ctz = Atomic<Opcode::I32_ctz>;
+using I32_popcnt = Atomic<Opcode::I32_popcnt>;
+using I32_add = Atomic<Opcode::I32_add>;
+using I32_sub = Atomic<Opcode::I32_sub>;
+using I32_mul = Atomic<Opcode::I32_mul>;
+using I32_div_s = Atomic<Opcode::I32_div_s>;
+using I32_div_u = Atomic<Opcode::I32_div_u>;
+using I32_rem_s = Atomic<Opcode::I32_rem_s>;
+using I32_rem_u = Atomic<Opcode::I32_rem_u>;
+using I32_and = Atomic<Opcode::I32_and>;
+using I32_or = Atomic<Opcode::I32_or>;
+using I32_xor = Atomic<Opcode::I32_xor>;
+using I32_shl = Atomic<Opcode::I32_shl>;
+using I32_shr_s = Atomic<Opcode::I32_shr_s>;
+using I32_shr_u = Atomic<Opcode::I32_shr_u>;
+using I32_rotl = Atomic<Opcode::I32_rotl>;
+using I32_rotr = Atomic<Opcode::I32_rotr>;
+using I64_clz = Atomic<Opcode::I64_clz>;
+using I64_ctz = Atomic<Opcode::I64_ctz>;
+using I64_popcnt = Atomic<Opcode::I64_popcnt>;
+using I64_add = Atomic<Opcode::I64_add>;
+using I64_sub = Atomic<Opcode::I64_sub>;
+using I64_mul = Atomic<Opcode::I64_mul>;
+using I64_div_s = Atomic<Opcode::I64_div_s>;
+using I64_div_u = Atomic<Opcode::I64_div_u>;
+using I64_rem_s = Atomic<Opcode::I64_rem_s>;
+using I64_rem_u = Atomic<Opcode::I64_rem_u>;
+using I64_and = Atomic<Opcode::I64_and>;
+using I64_or = Atomic<Opcode::I64_or>;
+using I64_xor = Atomic<Opcode::I64_xor>;
+using I64_shl = Atomic<Opcode::I64_shl>;
+using I64_shr_s = Atomic<Opcode::I64_shr_s>;
+using I64_shr_u = Atomic<Opcode::I64_shr_u>;
+using I64_rotl = Atomic<Opcode::I64_rotl>;
+using I64_rotr = Atomic<Opcode::I64_rotr>;
+using F32_abs = Atomic<Opcode::F32_abs>;
+using F32_neg = Atomic<Opcode::F32_neg>;
+using F32_ceil = Atomic<Opcode::F32_ceil>;
+using F32_floor = Atomic<Opcode::F32_floor>;
+using F32_trunc = Atomic<Opcode::F32_trunc>;
+using F32_nearest = Atomic<Opcode::F32_nearest>;
+using F32_sqrt = Atomic<Opcode::F32_sqrt>;
+using F32_add = Atomic<Opcode::F32_add>;
+using F32_sub = Atomic<Opcode::F32_sub>;
+using F32_mul = Atomic<Opcode::F32_mul>;
+using F32_div = Atomic<Opcode::F32_div>;
+using F32_min = Atomic<Opcode::F32_min>;
+using F32_max = Atomic<Opcode::F32_max>;
+using F32_copysign = Atomic<Opcode::F32_copysign>;
+using F64_abs = Atomic<Opcode::F64_abs>;
+using F64_neg = Atomic<Opcode::F64_neg>;
+using F64_ceil = Atomic<Opcode::F64_ceil>;
+using F64_floor = Atomic<Opcode::F64_floor>;
+using F64_trunc = Atomic<Opcode::F64_trunc>;
+using F64_nearest = Atomic<Opcode::F64_nearest>;
+using F64_sqrt = Atomic<Opcode::F64_sqrt>;
+using F64_add = Atomic<Opcode::F64_add>;
+using F64_sub = Atomic<Opcode::F64_sub>;
+using F64_mul = Atomic<Opcode::F64_mul>;
+using F64_div = Atomic<Opcode::F64_div>;
+using F64_min = Atomic<Opcode::F64_min>;
+using F64_max = Atomic<Opcode::F64_max>;
+using F64_copysign = Atomic<Opcode::F64_copysign>;
+using I32_wrap_i64 = Atomic<Opcode::I32_wrap_i64>;
+using I32_trunc_s_f32 = Atomic<Opcode::I32_trunc_s_f32>;
+using I32_trunc_u_f32 = Atomic<Opcode::I32_trunc_u_f32>;
+using I32_trunc_s_f64 = Atomic<Opcode::I32_trunc_s_f64>;
+using I32_trunc_u_f64 = Atomic<Opcode::I32_trunc_u_f64>;
+using I64_extend_s_i32 = Atomic<Opcode::I64_extend_s_i32>;
+using I64_extend_u_i32 = Atomic<Opcode::I64_extend_u_i32>;
+using I64_trunc_s_f32 = Atomic<Opcode::I64_trunc_s_f32>;
+using I64_trunc_u_f32 = Atomic<Opcode::I64_trunc_u_f32>;
+using I64_trunc_s_f64 = Atomic<Opcode::I64_trunc_s_f64>;
+using I64_trunc_u_f64 = Atomic<Opcode::I64_trunc_u_f64>;
+using F32_convert_s_i32 = Atomic<Opcode::F32_convert_s_i32>;
+using F32_convert_u_i32 = Atomic<Opcode::F32_convert_u_i32>;
+using F32_convert_s_i64 = Atomic<Opcode::F32_convert_s_i64>;
+using F32_convert_u_i64 = Atomic<Opcode::F32_convert_u_i64>;
+using F32_demote_f64 = Atomic<Opcode::F32_demote_f64>;
+using F64_convert_s_i32 = Atomic<Opcode::F64_convert_s_i32>;
+using F64_convert_u_i32 = Atomic<Opcode::F64_convert_u_i32>;
+using F64_convert_s_i64 = Atomic<Opcode::F64_convert_s_i64>;
+using F64_convert_u_i64 = Atomic<Opcode::F64_convert_u_i64>;
+using F64_promote_f32 = Atomic<Opcode::F64_promote_f32>;
+using I32_reinterpret_f32 = Atomic<Opcode::I32_reinterpret_f32>;
+using I64_reinterpret_f64 = Atomic<Opcode::I64_reinterpret_f64>;
+using F32_reinterpret_i32 = Atomic<Opcode::F32_reinterpret_i32>;
+using F64_reinterpret_i64 = Atomic<Opcode::F64_reinterpret_i64>;
+using I32_extend8_s = Atomic<Opcode::I32_extend8_s>;
+using I32_extend16_s = Atomic<Opcode::I32_extend16_s>;
+using I64_extend8_s = Atomic<Opcode::I64_extend8_s>;
+using I64_extend16_s = Atomic<Opcode::I64_extend16_s>;
+using I64_extend32_s = Atomic<Opcode::I64_extend32_s>;
+using I32_trunc_sat_f32_s = Atomic<Opcode::I32_trunc_sat_f32_s>;
+using I32_trunc_sat_f32_u = Atomic<Opcode::I32_trunc_sat_f32_u>;
+using I32_trunc_sat_f64_s = Atomic<Opcode::I32_trunc_sat_f64_s>;
+using I32_trunc_sat_f64_u = Atomic<Opcode::I32_trunc_sat_f64_u>;
+using I64_trunc_sat_f32_s = Atomic<Opcode::I64_trunc_sat_f32_s>;
+using I64_trunc_sat_f32_u = Atomic<Opcode::I64_trunc_sat_f32_u>;
+using I64_trunc_sat_f64_s = Atomic<Opcode::I64_trunc_sat_f64_s>;
+using I64_trunc_sat_f64_u = Atomic<Opcode::I64_trunc_sat_f64_u>;
 
 }
 
@@ -251,7 +387,143 @@ using WasmInstr = std::variant<
     Instr::I32_const,
     Instr::I64_const,
     Instr::F32_const,
-    Instr::F64_const
+    Instr::F64_const,
+    Instr::I32_eqz,
+    Instr::I32_eq,
+    Instr::I32_ne,
+    Instr::I32_lt_s,
+    Instr::I32_lt_u,
+    Instr::I32_gt_s,
+    Instr::I32_gt_u,
+    Instr::I32_le_s,
+    Instr::I32_le_u,
+    Instr::I32_ge_s,
+    Instr::I32_ge_u,
+    Instr::I64_eqz,
+    Instr::I64_eq,
+    Instr::I64_ne,
+    Instr::I64_lt_s,
+    Instr::I64_lt_u,
+    Instr::I64_gt_s,
+    Instr::I64_gt_u,
+    Instr::I64_le_s,
+    Instr::I64_le_u,
+    Instr::I64_ge_s,
+    Instr::I64_ge_u,
+    Instr::F32_eq,
+    Instr::F32_ne,
+    Instr::F32_lt,
+    Instr::F32_gt,
+    Instr::F32_le,
+    Instr::F32_ge,
+    Instr::F64_eq,
+    Instr::F64_ne,
+    Instr::F64_lt,
+    Instr::F64_gt,
+    Instr::F64_le,
+    Instr::F64_ge,
+    Instr::I32_clz,
+    Instr::I32_ctz,
+    Instr::I32_popcnt,
+    Instr::I32_add,
+    Instr::I32_sub,
+    Instr::I32_mul,
+    Instr::I32_div_s,
+    Instr::I32_div_u,
+    Instr::I32_rem_s,
+    Instr::I32_rem_u,
+    Instr::I32_and,
+    Instr::I32_or,
+    Instr::I32_xor,
+    Instr::I32_shl,
+    Instr::I32_shr_s,
+    Instr::I32_shr_u,
+    Instr::I32_rotl,
+    Instr::I32_rotr,
+    Instr::I64_clz,
+    Instr::I64_ctz,
+    Instr::I64_popcnt,
+    Instr::I64_add,
+    Instr::I64_sub,
+    Instr::I64_mul,
+    Instr::I64_div_s,
+    Instr::I64_div_u,
+    Instr::I64_rem_s,
+    Instr::I64_rem_u,
+    Instr::I64_and,
+    Instr::I64_or,
+    Instr::I64_xor,
+    Instr::I64_shl,
+    Instr::I64_shr_s,
+    Instr::I64_shr_u,
+    Instr::I64_rotl,
+    Instr::I64_rotr,
+    Instr::F32_abs,
+    Instr::F32_neg,
+    Instr::F32_ceil,
+    Instr::F32_floor,
+    Instr::F32_trunc,
+    Instr::F32_nearest,
+    Instr::F32_sqrt,
+    Instr::F32_add,
+    Instr::F32_sub,
+    Instr::F32_mul,
+    Instr::F32_div,
+    Instr::F32_min,
+    Instr::F32_max,
+    Instr::F32_copysign,
+    Instr::F64_abs,
+    Instr::F64_neg,
+    Instr::F64_ceil,
+    Instr::F64_floor,
+    Instr::F64_trunc,
+    Instr::F64_nearest,
+    Instr::F64_sqrt,
+    Instr::F64_add,
+    Instr::F64_sub,
+    Instr::F64_mul,
+    Instr::F64_div,
+    Instr::F64_min,
+    Instr::F64_max,
+    Instr::F64_copysign,
+    Instr::I32_wrap_i64,
+    Instr::I32_trunc_s_f32,
+    Instr::I32_trunc_u_f32,
+    Instr::I32_trunc_s_f64,
+    Instr::I32_trunc_u_f64,
+    Instr::I64_extend_s_i32,
+    Instr::I64_extend_u_i32,
+    Instr::I64_trunc_s_f32,
+    Instr::I64_trunc_u_f32,
+    Instr::I64_trunc_s_f64,
+    Instr::I64_trunc_u_f64,
+    Instr::F32_convert_s_i32,
+    Instr::F32_convert_u_i32,
+    Instr::F32_convert_s_i64,
+    Instr::F32_convert_u_i64,
+    Instr::F32_demote_f64,
+    Instr::F64_convert_s_i32,
+    Instr::F64_convert_u_i32,
+    Instr::F64_convert_s_i64,
+    Instr::F64_convert_u_i64,
+    Instr::F64_promote_f32,
+    Instr::I32_reinterpret_f32,
+    Instr::I64_reinterpret_f64,
+    Instr::F32_reinterpret_i32,
+    Instr::F64_reinterpret_i64,
+    Instr::I32_extend8_s,
+    Instr::I32_extend16_s,
+    Instr::I64_extend8_s,
+    Instr::I64_extend16_s,
+    Instr::I64_extend32_s,
+    Instr::I32_trunc_sat_f32_s,
+    Instr::I32_trunc_sat_f32_u,
+    Instr::I32_trunc_sat_f64_s,
+    Instr::I32_trunc_sat_f64_u,
+    Instr::I64_trunc_sat_f32_s,
+    Instr::I64_trunc_sat_f32_u,
+    Instr::I64_trunc_sat_f64_s,
+    Instr::I64_trunc_sat_f64_u
 >;
 
 }
--- src/lib/parse/Keywords.hpp
+++ src/lib/parse/Keywords.hpp
@@ -21,6 +21,17 @@ static std::unordered_set<std::string> keywords = {
     "i64.load8_s","i64.load8_u","i64.load16_s","i64.load16_u","i64.load32_s","i64.load32_u",
     "i32.store","i64.store","f32.store","f64.store","i32.store8","i32.store16","i64.store8","i64.store16","i64.store32",
     "i32.const","i64.const","f32.const","f64.const",
+    "i32.eqz", "i32.eq", "i32.ne", "i32.lt_s", "i32.lt_u", "i32.gt_s", "i32.gt_u", "i32.le_s", "i32.le_u", "i32.ge_s", "i32.ge_u",
+    "i64.eqz", "i64.eq", "i64.ne", "i64.lt_s", "i64.lt_u", "i64.gt_s", "i64.gt_u", "i64.le_s", "i64.le_u", "i64.ge_s", "i64.ge_u",
+    "f32.eq", "f32.ne", "f32.lt", "f32.gt", "f32.le", "f32.ge", "f64.eq", "f64.ne", "f64.lt", "f64.gt", "f64.le", "f64.ge",
+    "i32.clz", "i32.ctz", "i32.popcnt", "i32.add", "i32.sub", "i32.mul", "i32.div_s", "i32.div_u", "i32.rem_s", "i32.rem_u", "i32.and", "i32.or", "i32.xor", "i32.shl", "i32.shr_s", "i32.shr_u", "i32.rotl", "i32.rotr",
+    "i64.clz", "i64.ctz", "i64.popcnt", "i64.add", "i64.sub", "i64.mul", "i64.div_s", "i64.div_u", "i64.rem_s", "i64.rem_u", "i64.and", "i64.or", "i64.xor", "i64.shl", "i64.shr_s", "i64.shr_u", "i64.rotl", "i64.rotr", 
+    "f32.abs", "f32.neg", "f32.ceil", "f32.floor", "f32.trunc", "f32.nearest", "f32.sqrt", "f32.add", "f32.sub", "f32.mul", "f32.div", "f32.min", "f32.max", "f32.copysign", 
+    "f64.abs", "f64.neg", "f64.ceil", "f64.floor", "f64.trunc", "f64.nearest", "f64.sqrt", "f64.add", "f64.sub", "f64.mul", "f64.div", "f64.min", "f64.max", "f64.copysign", 
+    "i32.wrap_i64", "i32.trunc_s_f32", "i32.trunc_u_f32", "i32.trunc_s_f64", "i32.trunc_u_f64", "i64.extend_s_i32", "i64.extend_u_i32", "i64.trunc_s_f32", "i64.trunc_u_f32", "i64.trunc_s_f64", "i64.trunc_u_f64", 
+    "f32.convert_s_i32", "f32.convert_u_i32", "f32.convert_s_i64", "f32.convert_u_i64", "f32.demote_f64", "f64.convert_s_i32", "f64.convert_u_i32", "f64.convert_s_i64", "f64.convert_u_i64", "f64.promote_f32", 
+    "i32.reinterpret_f32", "i64.reinterpret_f64", "f32.reinterpret_i32", "f64.reinterpret_i64", "i32.extend8_s", "i32.extend16_s", "i64.extend8_s", "i64.extend16_s", "i64.extend32_s", 
+    "i32.trunc_sat_f32_s", "i32.trunc_sat_f32_u", "i32.trunc_sat_f64_s", "i32.trunc_sat_f64_u", "i64.trunc_sat_f32_s", "i64.trunc_sat_f32_u", "i64.trunc_sat_f64_s", "i64.trunc_sat_f64_u",
 };
 
 #endif
