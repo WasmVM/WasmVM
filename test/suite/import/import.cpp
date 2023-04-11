dcc13610381bd6a7e1c26a4c4ff9ee639f460004
LuisHsu <luishsuplus@gmail.com>
2023-04-10 23:59:40 -0700

    Update import table tests

--- test/suite/import/import.cpp
+++ test/suite/import/import.cpp
@@ -12,7 +12,6 @@ using namespace Testing;
 Suite import {
     Test("func", {
         ParseFile(test_module, "func.wat");
-        std::cout << module_dump(test_module) << std::endl;
         // types
         FuncType& type0 = test_module.types[0];
         Expect(type0.params.size() == 0);
@@ -52,7 +51,25 @@ Suite import {
         Expect(std::get<index_t>(imp4.desc) == 4);
     })
     Test("table", {
-
+        ParseFile(test_module, "table.wat");
+        WasmImport& imp0 = test_module.imports[0];
+        Expect(std::holds_alternative<TableType>(imp0.desc));
+        TableType& table0 = std::get<TableType>(imp0.desc);
+        Expect(table0.limits.min == 2);
+        Expect(table0.limits.max && table0.limits.max.value() == 3);
+        Expect(table0.reftype == RefType::funcref);
+        WasmImport& imp1 = test_module.imports[1];
+        Expect(std::holds_alternative<TableType>(imp1.desc));
+        TableType& table1 = std::get<TableType>(imp1.desc);
+        Expect(table1.limits.min == 4);
+        Expect(table1.limits.max && table0.limits.max.value() == 5);
+        Expect(table1.reftype == RefType::externref);
+        WasmImport& imp2 = test_module.imports[2];
+        Expect(std::holds_alternative<TableType>(imp2.desc));
+        TableType& table2 = std::get<TableType>(imp2.desc);
+        Expect(table1.limits.min == 6);
+        Expect(!table1.limits.max);
+        Expect(table1.reftype == RefType::funcref);
     })
     Test("memory", {
 
---
+++ test/suite/import/table.wat
@@ -0,0 +1,5 @@
+(module
+    (import "testmod" "table0" (table 2 3 funcref))
+    (import "testmod" "table1" (table 4 5 externref))
+    (import "testmod" "table2" (table 6 funcref))
+)
