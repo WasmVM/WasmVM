8ab08f2a46018a82f32da1f326846b8a453c9f2e
LuisHsu <luishsuplus@gmail.com>
2023-05-06 16:09:03 -0700

    Retire C API

--- test/harness/md5.h
+++
@@ -1,8 +0,0 @@
-#ifndef WASMVM_TEST_MD5_DEF
-#define WASMVM_TEST_MD5_DEF
-
-#include <defines.h>
-
-void md5_hash(const u32_t data_size, const byte_t data[data_size], u64_t result[2]);
-
-#endif
--- test/harness/utils.h
+++
@@ -1,9 +0,0 @@
-#ifndef WASMVM_TEST_UTILS_DEF
-#define WASMVM_TEST_UTILS_DEF
-
-#include <Stack.h>
-
-void _dump_stack(wasm_stack stack);
-void _dump_frame(wasm_stack frame);
-
-#endif
