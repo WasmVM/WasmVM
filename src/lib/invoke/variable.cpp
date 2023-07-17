64cf6070c040f3c863376a326219a2b6d3d790e7
LuisHsu <luishsuplus@gmail.com>
2023-07-16 22:37:22 -0700

    Execute variable instructions

--- src/lib/invoke/variable.cpp
+++ src/lib/invoke/variable.cpp
@@ -9,18 +9,33 @@
 
 using namespace WasmVM;
 
-void RunVisitor::operator()(Instr::Local_get&){
-    // TODO:
+void RunVisitor::operator()(Instr::Local_get& instr){
+  Frame& frame = stack.frames.top();
+  Label& label = frame.labels.top();
+  label.values.emplace(frame.locals[instr.index]);
 }
-void RunVisitor::operator()(Instr::Local_set&){
-    // TODO:
+void RunVisitor::operator()(Instr::Local_set& instr){
+  Frame& frame = stack.frames.top();
+  Label& label = frame.labels.top();
+  frame.locals[instr.index] = label.values.top();
+  label.values.pop();
 }
-void RunVisitor::operator()(Instr::Local_tee&){
-    // TODO:
+void RunVisitor::operator()(Instr::Local_tee& instr){
+  Frame& frame = stack.frames.top();
+  Label& label = frame.labels.top();
+  frame.locals[instr.index] = label.values.top();
 }
-void RunVisitor::operator()(Instr::Global_get&){
-    // TODO:
+void RunVisitor::operator()(Instr::Global_get& instr){
+  Frame& frame = stack.frames.top();
+  Label& label = frame.labels.top();
+  index_t globaladdr = frame.module.globaladdrs[instr.index];
+  label.values.emplace(stack.store.globals[globaladdr].value);
 }
-void RunVisitor::operator()(Instr::Global_set&){
-    // TODO:
+void RunVisitor::operator()(Instr::Global_set& instr){
+  Frame& frame = stack.frames.top();
+  Label& label = frame.labels.top();
+  index_t globaladdr = frame.module.globaladdrs[instr.index];
+  stack.store.globals[globaladdr].value = label.values.top();
+  stack.store.globals[globaladdr].type = (ValueType)label.values.top().index();
+  label.values.pop();
 }
