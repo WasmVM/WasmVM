b7511e0fff379a9264499585c48b7a9b80d2b387
LuisHsu <luishsuplus@gmail.com>
2024-07-14 20:51:43 -0700

    [parse] Start implement parser

--- src/lib/parse/test.cpp
+++ src/lib/parse/test.cpp
@@ -1,16 +1,17 @@
 #include <iostream>
 #include <fstream>
 #include <exception.hpp>
+#include <WasmVM.hpp>
 #include "Lexer.hpp"
+#include "Parser.hpp"
+#include "../dump/dump.hpp"
 
 int main(int argc, char const *argv[]){
     std::ifstream fin(argv[1]);
-    WasmVM::Lexer lexer(argv[1], fin);
     try{
-        WasmVM::Token token = lexer.get();
-        while(!std::holds_alternative<std::monostate>(token)){
-            token = lexer.get();
-        }
+        WasmVM::Lexer lexer(argv[1], fin);
+        WasmVM::Parser parser(lexer);
+        WasmVM::module_dump(parser.parse(), std::cout);
     }catch(WasmVM::Exception::Parse& e){
         std::cerr << argv[1] << ":" << e.location.first << ":" << e.location.second << " error: " << e.what() << std::endl;
     }catch(WasmVM::UnknownToken& e){
