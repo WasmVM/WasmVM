fcac55cee37a6ab40fe812c78b71c2aba27378bf
LuisHsu <luishsuplus@gmail.com>
2024-09-09 20:31:53 -0700

    [parse] active elem section

---
+++ test/suite/elem/active.wat
@@ -0,0 +1,7 @@
+(module
+    (elem (table 1) (offset i32.const 2) func 3)
+    (elem $elem1 (table 5) (offset i32.const 6) func)
+    (elem (table 7) (i32.const 8) func)
+    (elem (offset i32.const 9) func 10)
+    (elem (i32.const 11) 12 13)
+)
--- test/suite/elem/elem.cpp
+++ test/suite/elem/elem.cpp
@@ -55,4 +55,51 @@ Suite elem {
         Instr::Ref_func& instr5_1 = std::get<Instr::Ref_func>(elem5.elemlist[1]);
         Expect(instr5_1.index == 7);
     })
+    Test("active", {
+        ParseFile(test_module, "active.wat");
+
+        WasmElem& elem0 = test_module.elems[0];
+        Expect(elem0.type == RefType::funcref);
+        Expect(elem0.mode.type == WasmElem::ElemMode::Mode::active);
+        Expect(elem0.mode.tableidx.value() == 1);
+        Expect(std::get<Instr::I32_const>(elem0.mode.offset.value()).value == 2);
+        Expect(elem0.elemlist.size() == 1);
+        Instr::Ref_func& instr0 = std::get<Instr::Ref_func>(elem0.elemlist[0]);
+        Expect(instr0.index == 3);
+
+        WasmElem& elem1 = test_module.elems[1];
+        Expect(elem1.type == RefType::funcref);
+        Expect(elem1.mode.type == WasmElem::ElemMode::Mode::active);
+        Expect(elem1.mode.tableidx.value() == 5);
+        Expect(std::get<Instr::I32_const>(elem1.mode.offset.value()).value == 6);
+        Expect(elem1.elemlist.empty());
+
+        WasmElem& elem2 = test_module.elems[2];
+        Expect(elem2.type == RefType::funcref);
+        Expect(elem2.mode.type == WasmElem::ElemMode::Mode::active);
+        Expect(elem2.mode.tableidx.value() == 7);
+        Expect(std::get<Instr::I32_const>(elem2.mode.offset.value()).value == 8);
+        Expect(elem2.elemlist.empty());
+
+        WasmElem& elem3 = test_module.elems[3];
+        Expect(elem3.type == RefType::funcref);
+        Expect(elem3.mode.type == WasmElem::ElemMode::Mode::active);
+        Expect(elem3.mode.tableidx.value() == 0);
+        Expect(std::get<Instr::I32_const>(elem3.mode.offset.value()).value == 9);
+        Expect(elem3.elemlist.size() == 1);
+        Instr::Ref_func& instr3 = std::get<Instr::Ref_func>(elem3.elemlist[0]);
+        Expect(instr3.index == 10);
+
+        WasmElem& elem4 = test_module.elems[4];
+        Expect(elem4.type == RefType::funcref);
+        Expect(elem4.mode.type == WasmElem::ElemMode::Mode::active);
+        Expect(elem4.mode.tableidx.value() == 0);
+        Expect(std::get<Instr::I32_const>(elem4.mode.offset.value()).value == 11);
+        Expect(elem4.elemlist.size() == 2);
+        Instr::Ref_func& instr4_0 = std::get<Instr::Ref_func>(elem4.elemlist[0]);
+        Expect(instr4_0.index == 12);
+        Instr::Ref_func& instr4_1 = std::get<Instr::Ref_func>(elem4.elemlist[1]);
+        Expect(instr4_1.index == 13);
+
+    })
 };
