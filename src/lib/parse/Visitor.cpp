36a83a1d66185491b0e0b173dbbb8140d5f10a71
LuisHsu <luishsuplus@gmail.com>
2024-10-14 22:37:56 -0700

    [Parse] Visit type section

--- src/lib/parse/Visitor.cpp
+++ src/lib/parse/Visitor.cpp
@@ -5,9 +5,73 @@
 #include "Visitor.hpp"
 #include <WasmVM.hpp>
 
+#include <algorithm>
+
 using namespace WasmVM;
 
-std::any Visitor::visitModule(WatParser::ModuleContext *context){
-    visitChildren(context);
+WasmModule Visitor::visit(WatParser::ModuleContext *ctx){
+    visitModule(ctx);
+    // TODO: post process
     return module;
+}
+
+std::any Visitor::visitModule(WatParser::ModuleContext *ctx){
+    visitChildren(ctx);
+    // types
+    std::transform(types.begin(), types.end(), std::back_inserter(module.types), [](auto& type_pair){
+        return type_pair.first;
+    });
+    return module;
+}
+
+std::any Visitor::visitModulefield(WatParser::ModulefieldContext *ctx){
+    return visitChildren(ctx);
+}
+
+std::any Visitor::visitTypesection(WatParser::TypesectionContext *ctx){
+    if(ctx->Id() != nullptr){
+        type_map[ctx->Id()->getText()] = types.size();
+    }
+    std::pair<FuncType, std::map<std::string, index_t>> functype = std::any_cast<std::pair<FuncType, std::map<std::string, index_t>>>(visitFunctype(ctx->functype()));
+    return types.emplace_back(functype);
+}
+
+std::any Visitor::visitValtype(WatParser::ValtypeContext *ctx){
+    return visitChildren(ctx);
+}
+
+std::any Visitor::visitFunctype(WatParser::FunctypeContext *ctx){
+    std::pair<FuncType, std::map<std::string, index_t>> functype;
+    // Param
+    for(auto param_tree : ctx->param()){
+        std::variant<std::pair<std::string, ValueType>, std::vector<ValueType>> param = std::any_cast<std::variant<std::pair<std::string, ValueType>, std::vector<ValueType>>>(visitParam(param_tree));
+        std::visit(overloaded {
+            [&](std::pair<std::string, ValueType>& param_pair){
+                functype.second[param_pair.first] = functype.first.params.size();
+                functype.first.params.emplace_back(param_pair.second);
+            },
+            [&](std::vector<ValueType>& params){
+                functype.first.params.insert(functype.first.params.end(), params.begin(), params.end());
+            }
+        }, param);
+    }
+    // Result
+    return functype;
+}
+std::any Visitor::visitParam(WatParser::ParamContext *ctx){
+    std::variant<std::pair<std::string, ValueType>, std::vector<ValueType>> param;
+    if(ctx->Id() != nullptr){
+        std::string id = ctx->Id()->getText();
+        ValueType valuetype = std::any_cast<ValueType>(visitValtype(ctx->valtype(0)));
+        param = std::pair<std::string, ValueType> {id, valuetype};
+    }else{
+        std::vector<ValueType>& valuetypes = param.emplace<std::vector<ValueType>>();
+        for(auto valuetype : ctx->valtype()){
+            valuetypes.emplace_back(std::any_cast<ValueType>(visitValtype(valuetype)));
+        }
+    }
+    return param;
+}
+std::any Visitor::visitResult(WatParser::ResultContext *ctx){
+    return visitChildren(ctx);
 }
--- src/lib/parse/Visitor.hpp
+++ src/lib/parse/Visitor.hpp
@@ -1,17 +1,29 @@
 #ifndef WASMVM_Parse_Visitor
 #define WASMVM_Parse_Visitor
 
-#include <WatBaseVisitor.h>
+#include <parse/WatBaseVisitor.h>
 #include <WasmVM.hpp>
+
+#include <map>
 
 namespace WasmVM {
 
-struct Visitor : WatBaseVisitor {
-
+class Visitor : WatBaseVisitor {
+protected:
     WasmModule module;
+    std::map<std::string, index_t> type_map;
+    std::vector<std::pair<FuncType, std::map<std::string, index_t>>> types;
 
-    virtual std::any visitModule(WatParser::ModuleContext *context);
+    virtual std::any visitModulefield(WatParser::ModulefieldContext *ctx) override;
+    virtual std::any visitTypesection(WatParser::TypesectionContext *ctx) override;
+    virtual std::any visitValtype(WatParser::ValtypeContext *ctx) override;
+    virtual std::any visitFunctype(WatParser::FunctypeContext *ctx) override;
+    virtual std::any visitParam(WatParser::ParamContext *ctx) override;
+    virtual std::any visitResult(WatParser::ResultContext *ctx) override;
+    virtual std::any visitModule(WatParser::ModuleContext *ctx) override;
 
+public:
+    WasmModule visit(WatParser::ModuleContext *ctx);
 };
 
 }
